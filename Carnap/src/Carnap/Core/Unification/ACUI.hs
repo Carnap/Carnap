850e0d551a2894956088e131a82af2f84db825b2
JakeEhrlich <jake.h.ehrlich@gmail.com>
2016-03-18 05:13:00 -0500

    Added description of my algorithm so I don't forget

--- Carnap/src/Carnap/Core/Unification/ACUI.hs
+++ Carnap/src/Carnap/Core/Unification/ACUI.hs
@@ -12,13 +12,15 @@ class SatProblem t where
     unassigned :: t -> [Int]
     maxLiteral :: t -> Int
     valuation :: t -> [Int]
+    solutions :: t -> [[Int]]
 
 data IntMapSat = IntMapSat {
   imValuation :: [Int]
   imUnassigned :: [Int],
   imUnits :: [Int],
   imProb :: IntMap [Int],
-  imMaxLit :: Int
+  imMaxLit :: Int,
+  imSolutions :: [[Int]]
 }
 
 --a SatProblem instance with constant time for all operations except unitProp
@@ -28,6 +30,7 @@ instance SatProblem IntMapSat where
     unassigned = imUnassigned
     maxLiteral = imMaxLit
     valuation = imValuation
+    solutions = imSolutions
     unitProp x prob | single ns = Nothing
                     | otherwise = IntMapSat valu unassign myunits res (maxLiteral prob)
         where t = imProb prob
@@ -45,3 +48,16 @@ instance SatProblem IntMapSat where
 --other solutions
 compSols :: [Int] -> [Int] -> Bool
 compSols x y = not . null $ (filter (> 0) y) // (filter (> 0) x)
+
+
+--the basic algorithm I came up with is based on DPLL
+
+--1. cascade all units until there are no more units
+--2. select a literal and call l. the order in which literals is selected is
+--   important to performance. choose varibles to go early that have a higher
+--   chance of causing unit cascades when assigned false
+--3. assign that literal false and find all solutions
+--4. given the current assignment and solutons determine weather it is worth it
+--   to search for this 1. if adding this to valuation dominates all solutions
+--   then there is no point in searching because all further solutions will be
+--   bigger than one of the current solutions
