c926145702ea14e653d144ab020d6dd3067f7415
JakeEhrlich <jake.h.ehrlich@gmail.com>
2016-03-22 18:10:38 -0500

    Checkpointing my work to play with plated branch

--- Carnap/src/Carnap/Core/Unification/ACUI.hs
+++ Carnap/src/Carnap/Core/Unification/ACUI.hs
@@ -1,54 +1,51 @@
-module ACUI () where
+module Carnap.Core.Unification.ACUI () where
 
 import Data.List
-import qualified Data.IntMap.Lazy as IntMap
+import Control.Monad
+import Control.Monad.State
 
 --to solve ACUI unification with constants we need to be able to find
 --all minimal solutions to a SAT problem
 
+--there are different representations of a SatProblem that be more efficent
+--because I don't know which will be most efficent and efficency might be a
+--concern at some point I created a type class to represent what a SAT problem
+--should be able to do so that the DPLL algorithm is independent of the SAT
+--representation
 class SatProblem t where
-    unitProp :: Int -> t -> Maybe t
+    unitProp :: Int -> t -> t
     units :: t -> [Int]
     unassigned :: t -> [Int]
-    maxLiteral :: t -> Int
     valuation :: t -> [Int]
-    solutions :: t -> [[Int]]
+    evaluate :: t -> Bool
 
-data IntMapSat = IntMapSat {
-  imValuation :: [Int]
-  imUnassigned :: [Int],
-  imUnits :: [Int],
-  imProb :: IntMap [Int],
-  imMaxLit :: Int,
-  imSolutions :: [[Int]]
-}
+data ListSat = ListSat [Int] [Int] [[Int]]
+             | ListSatFalse
 
---a SatProblem instance with constant time for all operations except unitProp
---which is a kinda expensive logrithmic factor
-instance SatProblem IntMapSat where
-    units = imUnits
-    unassigned = imUnassigned
-    maxLiteral = imMaxLit
-    valuation = imValuation
-    solutions = imSolutions
-    unitProp x prob | single ns = Nothing
-                    | otherwise = IntMapSat valu unassign myunits res (maxLiteral prob)
-        where t = imProb prob
-              toDelete = t ! x
-              (Just ns, noneg) = insertLookupWithKey (\_ _ -> []) (negate x) [] t
-              res = IntMap.map (\\ toDelete) noneg
-              single [x] = True
+instance SatProblem ListSat where
+    evaluate ListSatFalse       = False
+    evaluate (ListSat vs [] cs) = all (any (`elem` vs)) cs
+    evaluate _                  = error "You cannot evaluatae a term with unassigned literals"
+
+    valuation ListSatFalse     = []
+    valuation (ListSat vs _ _) = vs
+
+    unassigned ListSatFalse     = []
+    unassigned (ListSat _ us _) = us
+
+    units ListSatFalse     = []
+    units (ListSat _ _ cs) = map head . filter single $ cs
+        where single [_] = True
               single _   = False
-              valu = ([x] `union` valuation prob)
-              unassign = delete x (unassigned prob)
-              myunits = delete x (units prob)
 
---compSols x y == false iff x >= y
---you should add x to a set of minimal solutions if x compSols x y holds for all
---other solutions
-compSols :: [Int] -> [Int] -> Bool
-compSols x y = not . null $ (filter (> 0) y) // (filter (> 0) x)
-
+    unitProp _ ListSatFalse = ListSatFalse
+    unitProp u (ListSat vs us cs) = case csM of
+        Just cs' -> ListSat (u : vs) (delete (abs u) us) cs'
+        Nothing -> ListSatFalse
+        where csM = filterM pass . map (delete u) $ cs
+              pass clause = case clause of
+                  [] -> Nothing
+                  _ -> Just . not $ u `elem` clause
 
 --the basic algorithm I came up with is based on DPLL
 
@@ -61,3 +58,34 @@ compSols x y = not . null $ (filter (> 0) y) // (filter (> 0) x)
 --   to search for this 1. if adding this to valuation dominates all solutions
 --   then there is no point in searching because all further solutions will be
 --   bigger than one of the current solutions
+
+
+compSols :: [Int] -> [Int] -> Bool
+compSols x y = not . null $ (filter (> 0) y) \\ (filter (> 0) x)
+
+worthIt :: [Int] -> [[Int]] -> Bool
+worthIt proposal sols = all (compSols proposal) sols
+
+cascadeUnits :: SatProblem t => t -> t
+cascadeUnits s | null us   = s
+               | otherwise = cascadeUnits $ foldr unitProp s us
+               where us = units s
+
+--search :: SatProblem t => t -> State [[Int]] ()
+--search s = do
+    --let s' = cascadeUnits s --first flush out all of the units
+    --case unassigned s' of
+      --  (l:_) -> do search (unitProp (negate l) s')
+        --            vals <- get
+          --          search (unitProp l s') vals
+            --        return ()
+        --[]    -> return ()
+    --case unassigned s' where
+      --  (l:_) -> runIdentity $ do
+        --    let sols' = search (unitProp (negate l) s') sols
+          --  let vals = map valuation sols'
+            --let sols'' = search (unitProp l s') vals in
+            --if worthIt (l : valutation s') vals
+              --then sols'
+              --else sols' ++ sols''
+        --[]    -> if evaluate s' then [s'] else []
