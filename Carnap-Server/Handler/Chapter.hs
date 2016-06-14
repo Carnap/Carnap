2a979b1bbd7913af2892b68ddc9e1ad8e1a653b1
gleachkr <gleachkr@gmail.com>
2016-06-14 11:11:59 -0500

    Create fallback abs. path for markdown on server.

--- Carnap-Server/Handler/Chapter.hs
+++ Carnap-Server/Handler/Chapter.hs
@@ -2,12 +2,13 @@ module Handler.Chapter where
 
 import Import
 import Yesod.Markdown
+import System.Directory (getDirectoryContents, doesDirectoryExist)
 import Text.Hamlet                 (hamletFile)
 import qualified Data.CaseInsensitive as CI
 import qualified Data.Text.Encoding as TE
 
 getChapterR :: Int -> Handler Html
-getChapterR n = do content <- liftIO $ fmap markdownToHtml (markdownFromFile $ "book/chapter" ++ show n ++ ".pandoc")
+getChapterR n = do content <- liftIO $ chapter n 
                    case content of
                        Right html -> chapterLayout $ layout html
                        Left err -> defaultLayout $ layout (show err)
@@ -15,6 +16,11 @@ getChapterR n = do content <- liftIO $ fmap markdownToHtml (markdownFromFile $ "
                         <article>
                             #{c}
                        |]
+
+chapter n = do localbook <- doesDirectoryExist "book"
+               let path = (if localbook then "book/chapter" else "/root/book/chapter") 
+                                    ++  show n ++ ".pandoc"
+               fmap markdownToHtml (markdownFromFile path)
 
 chapterLayout widget = do
         master <- getYesod
