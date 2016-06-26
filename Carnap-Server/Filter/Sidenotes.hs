bc26378a72d5de0143cdb155704a674b7e07bd63
gleachkr <gleachkr@gmail.com>
2016-06-26 14:52:21 -0500

    Forgot to add the sidenote filter

---
+++ Carnap-Server/Filter/Sidenotes.hs
@@ -0,0 +1,38 @@
+module Filter.Sidenotes (makeSideNotes
+) where
+
+import Text.Pandoc
+import Prelude
+import Data.List (intersperse)
+import Control.Monad.State
+
+makeSideNotes :: Inline -> State Int Inline
+makeSideNotes i = do n <- get
+                     modify (1 +)
+                     return $ pureMakeSideNotes i n 
+
+pureMakeSideNotes :: Inline -> Int -> Inline
+pureMakeSideNotes i@(Note (Para ((Str (s:ss):is)):bs)) n = 
+        case s of
+            ':' -> Span nullAttr [rawHead n, processNote i]
+            _ -> i
+pureMakeSideNotes x _ = x
+
+toInline :: Block -> Inline 
+toInline (Plain xs) = Span nullAttr xs 
+toInline (Para xs) = Span nullAttr xs 
+toInline (CodeBlock a s) = Code a s
+toInline (RawBlock f s) = RawInline f s
+--not handling the other block constructors for now.
+toInline _ = Span nullAttr [] 
+
+breakInlines :: [Block] -> [Inline]
+breakInlines = intersperse LineBreak . map toInline
+
+sidenoteAttr = ("",["sidenote"],[])
+
+processNote (Note (Para ((Str (s:ss):is)):bs)) = Span sidenoteAttr (breakInlines $ Para ((Str ss):is):bs)
+processNote i = i
+
+rawHead n = RawInline "html" $ "<label for=\"sn" ++ show n ++ "\" class=\"margin-toggle sidenote-number\"></label>"
+                                ++ "<input type=\"checkbox\" id=\"sn" ++ show n ++ "\" class=\"margin-toggle\"/>"
