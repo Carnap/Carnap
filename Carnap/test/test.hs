da510baee36859ff03292d90edc0c5183942fc47
gleachkr <gleachkr@gmail.com>
2016-07-23 11:45:27 -0500

    Extremely simple `combine` test

---
+++ Carnap/test/test.hs
@@ -0,0 +1,29 @@
+{-#LANGUAGE FlexibleContexts#-}
+
+module Main where
+
+import Carnap.Core.Unification.Unification
+import Carnap.Core.Unification.Combination
+import Carnap.Core.Examples.ACUI
+import System.CPUTime
+
+main = do putStrLn ""
+          t1 <- timeCombine 
+                    (parseTerm "f(a u b u c u d u e)") 
+                    (parseTerm "f(b u c u d u a)")
+          t2 <- timeCombine 
+                    (parseTerm "g(a u b u c u d u e)") 
+                    (parseTerm "f(b u c u d u a)")
+          putStrLn $ "Test Results (Positive Case):" ++ show t1 
+          putStrLn $ "Test Results (Negative Case):" ++ show t2
+          putStrLn ""
+
+time :: Fractional c => (a -> IO b) -> a -> IO c
+time action arg = do startTime <- getCPUTime
+                     action arg
+                     finishTime <- getCPUTime
+                     return $ fromIntegral (finishTime - startTime) / 1000000000000
+
+time' f = time (\x -> f x `seq` return ())
+
+timeCombine x y = time' (\z -> evalTerm $ combine [x :=: z]) y
