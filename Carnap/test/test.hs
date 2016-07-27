a76e94636680f193dbb6e388fc2d1d762c432568
gleachkr <gleachkr@gmail.com>
2016-07-27 15:35:56 -0500

    Add alternate combination method to tests

--- Carnap/test/test.hs
+++ Carnap/test/test.hs
@@ -12,25 +12,17 @@ import System.CPUTime
 import System.Exit (exitFailure)
 
 main = do putStrLn ""
-          testPositives [pseqcase1
-                        ,pseqcase2
-                        ,pseqcase3
-                        ,pseqcase4
-                        ,pseqcase5
-                        ,pseqcase6
-                        ]
-          testNegatives [nseqcase1
-                        ,nseqcase2
-                        ,nseqcase3
-                        ,nseqcase4
-                        ,nseqcase5
-                        ]
-          timeCombine [pacuicase1] "big positive acui" (posTest pacuicase1)
-          timeCombine [nacuicase1] "big negative acui" (negTest nacuicase1)
+          testPositives psequents
+          altTestPositives psequents
+          testNegatives nsequents
+          altTestNegatives nsequents
           timeCombine simpleModusPonens "positive modus ponens" (posTest "modus ponens")
           timeAltCombine simpleModusPonens "alt positive modus ponens" (posTest "modus ponens")
           timeCombine simpleModusPonens "positive modus ponens 2" (posTest "modus ponens")
           timeCombine simpleModusPonensErr "negative modus ponens" (negTest "modus ponens")
+          timeAltCombine simpleModusPonensErr "alt negative modus ponens" (negTest "modus ponens")
+          timeCombine [pacuicase1] "big positive acui" (posTest pacuicase1)
+          timeCombine [nacuicase1] "big negative acui" (negTest nacuicase1)
           putStrLn ""
 
 --test must be nontrivial or it optimizes away the test results
@@ -44,6 +36,7 @@ timeCombine eqs desc test = do startTime <- getCPUTime
 timeAltCombine eqs desc test = do startTime <- getCPUTime
                                   let subs = altCombine eqs
                                   test subs
+                                  putStrLn $ show subs
                                   finishTime <- getCPUTime
                                   let t = fromIntegral (finishTime - startTime) / 1000000000000
                                   putStrLn $ "Test Results (" ++ desc ++ "):" ++ show t
@@ -58,8 +51,12 @@ posTest eq = (testTemplate (not . null) (show eq ++ " as Positive" ))
 negTest eq = (testTemplate null (show eq ++ " as Negative" ))
 
 testNegatives = mapM (\eqs -> timeCombine [eqs] ("Negative test of " ++ show eqs) (negTest eqs))
+
+altTestNegatives = mapM (\eqs -> timeAltCombine [eqs] ("Alternative Negative test of " ++ show eqs) (negTest eqs))
                             
 testPositives = mapM (\eqs -> timeCombine [eqs] ("Positive test of " ++ show eqs) (posTest eqs))
+
+altTestPositives = mapM (\eqs -> timeAltCombine [eqs] ("Alternative Positive test of " ++ show eqs) (posTest eqs))
 
 pacuicase1 = (parseTerm "f(a u b u c u d u e)" :=: parseTerm "f(b u c u d u a)")
 
@@ -71,12 +68,26 @@ pseqcase3 = p :+: Top :|-: q :=: (p :|-: q)
 pseqcase4 = ((p :+: GammaV 1) :|-: q) :=: (p :|-: q)
 pseqcase5 = phi :+: GammaV 1 :|-: q :=: (p :|-: q)
 pseqcase6 = phi :+: phi :|-: q :=: (p :|-: q)
+psequents = [pseqcase1
+            ,pseqcase2
+            ,pseqcase3
+            ,pseqcase4
+            ,pseqcase5
+            ,pseqcase6
+            ]
+            
 
 nseqcase1 = p' :|-: q :=: (p :|-: q)
 nseqcase2 = p' :+: p :|-: q :=: (p :|-: q)
 nseqcase3 = ((p' :+: Top) :|-: q) :=: (p :|-: q)
 nseqcase4 = p' :+: Top :|-: q :=: (Top :|-: q)
 nseqcase5 = p :|-: q :=: (p :|-: Bot)
+nsequents = [nseqcase1
+            ,nseqcase2
+            ,nseqcase3
+            ,nseqcase4
+            ,nseqcase5
+            ]
 
 simpleModusPonensErr = [ ((GammaV 1) :|-: SS (phi_ :->-: phi'_)) :=: 
                          ((GammaV 2) :|-: SS (p_ :->-: p'_))
@@ -103,14 +114,18 @@ simpleModusPonens2 = [ ((GammaV 1) :|-: SS (phi_ :->-: phi'_)) :=:
                      ]
 
 altCombine :: [Equation PropSequentCalc] -> [[Equation PropSequentCalc]]
-altCombine eqs = evalTerm $ combine $ succs ++ head (evalTerm $ foUnifySys (const False) ants)
-        where succPair :: Equation PropSequentCalc -> Equation PropSequentCalc
-              succPair ((_ :|-: x):=:(_:|-: y))  = x :=: y
-              antPair :: Equation PropSequentCalc -> Equation PropSequentCalc
-              antPair  ((x :|-: _):=:(y:|-: _))  = x :=: y
+altCombine eqs = case evalTerm $ foUnifySys (const False) succs of
+                     [x] -> evalTerm $ combine $ ants ++ x
+                     [] -> []
+            where
               ants  = map antPair eqs
               succs = map succPair eqs
 
+succPair :: Equation PropSequentCalc -> Equation PropSequentCalc
+succPair ((_ :|-: x):=:(_:|-: y))  = x :=: y
+
+antPair :: Equation PropSequentCalc -> Equation PropSequentCalc
+antPair  ((x :|-: _):=:(y:|-: _))  = x :=: y
 
 p_ :: PropSequentCalc (Form Bool)
 p_ = SeqProp 1
