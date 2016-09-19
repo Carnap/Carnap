275c21ffe6fb13b12987c8993d41e530a6a9d11a
gleachkr <gleachkr@gmail.com>
2016-09-19 14:35:48 -0500

    cleanup of UserR handler

--- Carnap-Server/Handler/User.hs
+++ Carnap-Server/Handler/User.hs
@@ -13,7 +13,7 @@ getUserR userId = do
     let isAdmin = userId == "gleachkr@gmail.com"
     let pointsAvailable = "325" :: Text
     allUsers <- if isAdmin 
-                    then (runDB $ selectList [] []) >>= return . (map $ userIdent . fromEnt )
+                    then (runDB $ selectList [] []) >>= return . (map $ userIdent . entityVal)
                     else return []
     allScores <- mapM scoreById allUsers >>= return . zip allUsers
     defaultLayout $ do
@@ -48,7 +48,8 @@ getUserR userId = do
                         #{dueDateTable}
                     <div.col-md-3>
                         <h3> Total Points Earned
-                        <span style="font-size:56pt; color:gray; padding-left:20pt"> #{totalScore (synsubs ++ (transsubs ++ dersubs))}/#{pointsAvailable}
+                        <span style="font-size:56pt; color:gray; padding-left:20pt"> 
+                            #{totalScore (synsubs ++ (transsubs ++ dersubs))}/#{pointsAvailable}
                 <div>
                 <a href=@{AuthR LogoutR}>
                     Logout
@@ -56,6 +57,64 @@ getUserR userId = do
     where tryLookup l x = case lookup x l of
                           Just n -> show n
                           Nothing -> "can't find scores"
+
+--------------------------------------------------------
+--Grading
+--------------------------------------------------------
+--functions for calculating grades
+
+exPairToScore :: ((Text,Text),Text) -> Int
+exPairToScore ((x,y),z) =  case utcDueDate x of                      
+                              Just d -> if asUTC z `laterThan` d       
+                                            then 2
+                                            else 5
+                              Nothing -> 0
+
+scoreById uid = do (a,b,c) <- subsById uid
+                   return $ totalScore $ a ++ b ++ c
+
+totalScore xs = foldr (+) 0 (map exPairToScore xs)
+
+--------------------------------------------------------
+--Due dates
+--------------------------------------------------------
+--functions for dealing with time and due dates
+
+asUTC :: Text -> UTCTime
+asUTC z = read (unpack z)
+
+centralDaylight = TimeZone (-300) False "CDT"
+
+toKansasLocal z = ZonedTime (utcToLocalTime centralDaylight z) centralDaylight
+
+formatted z = formatTime defaultTimeLocale "%l:%M %P %Z, %a %b %e, %Y" (toKansasLocal z)
+
+utcDueDate x = M.lookup (read $ unpack (takeWhile (/= '.') x) :: Int) dueDates
+
+printDueDate x = case utcDueDate x of
+                Just d -> formatted d
+                Nothing -> "None"
+
+laterThan :: UTCTime -> UTCTime -> Bool
+laterThan t1 t2 = diffUTCTime t1 t2 > 0
+
+-- TODO: this should be pushed to a configuration file
+-- remember, the day clicks over at midnight.
+dueDates :: M.Map Int UTCTime
+dueDates = M.fromList [(1, toTime "11:59 pm CDT, Aug 30, 2016")
+                      ,(2, toTime "11:30 am CDT, Sep 1, 2016")
+                      ,(3, toTime "11:59 pm CDT, Sep 7, 2016")
+                      ,(4, toTime "11:59 pm CDT, Sep 7, 2016")
+                      ,(5, toTime "11:59 pm CDT, Sep 12, 2016")
+                      ,(6, toTime "11:59 pm CDT, Sep 14, 2016")
+                      ,(7, toTime "11:59 pm CDT, Sep 19, 2016")
+                      ]
+    where toTime = parseTimeOrError True defaultTimeLocale "%l:%M %P %Z, %b %e, %Y"
+
+--------------------------------------------------------
+--Blaze utility functions
+--------------------------------------------------------
+--functions for manipulating html
 
 textToUl :: [Text] -> Html
 textToUl xs = do B.ul $ mapM_ (B.li . B.toHtml) xs
@@ -81,38 +140,6 @@ exPairToTable xs = B.table B.! class_ "table table-striped" $ do
                                           B.td $ B.toHtml (formatted $ asUTC z)
                                           B.td $ B.toHtml $ show $ exPairToScore ((x,y),z)
 
-totalScore xs = foldr (+) 0 (map exPairToScore xs)
-
-scoreById uid = do (a,b,c) <- subsById uid
-                   return $ totalScore $ a ++ b ++ c
-
-subsById uid = do synsubs <- yourSubs synPair uid
-                  transsubs <- yourSubs transPair uid
-                  dersubs <- yourSubs derPair uid
-                  return (synsubs, transsubs, dersubs)
-
-exPairToScore :: ((Text,Text),Text) -> Int
-exPairToScore ((x,y),z) =  case utcDueDate x of                      
-                              Just d -> if asUTC z `laterThan` d       
-                                            then 2
-                                            else 5
-                              Nothing -> 0
-
-asUTC :: Text -> UTCTime
-asUTC z = read (unpack z)
-
-centralDaylight = TimeZone (-300) False "CDT"
-
-toKansasLocal z = ZonedTime (utcToLocalTime centralDaylight z) centralDaylight
-
-formatted z = formatTime defaultTimeLocale "%l:%M %P %Z, %a %b %e, %Y" (toKansasLocal z)
-
-utcDueDate x = M.lookup (read $ unpack (takeWhile (/= '.') x) :: Int) dueDates
-
-printDueDate x = case utcDueDate x of
-                Just d -> formatted d
-                Nothing -> "None"
-
 dueDateTable :: Html
 dueDateTable = B.table B.! class_ "table table-striped" $ do
                   B.thead $ do
@@ -122,27 +149,10 @@ dueDateTable = B.table B.! class_ "table table-striped" $ do
         where toRow (n, d) = B.tr $ do B.td $ B.toHtml $ show n
                                        B.td $ B.toHtml $ formatted d
 
-laterThan :: UTCTime -> UTCTime -> Bool
-laterThan t1 t2 = diffUTCTime t1 t2 > 0
-
--- TODO: this should be pushed to a configuration file
--- remember, the day clicks over at midnight.
-dueDates :: M.Map Int UTCTime
-dueDates = M.fromList [(1, toTime "11:59 pm CDT, Aug 30, 2016")
-                      ,(2, toTime "11:30 am CDT, Sep 1, 2016")
-                      ,(3, toTime "11:59 pm CDT, Sep 7, 2016")
-                      ,(4, toTime "11:59 pm CDT, Sep 7, 2016")
-                      ,(5, toTime "11:59 pm CDT, Sep 12, 2016")
-                      ,(6, toTime "11:59 pm CDT, Sep 14, 2016")
-                      ,(7, toTime "11:59 pm CDT, Sep 19, 2016")
-                      ]
-    where toTime = parseTimeOrError True defaultTimeLocale "%l:%M %P %Z, %b %e, %Y"
-
-yourSubs pair userId = do subs <- runDB $ selectList [] []
-                          let pairs = map (pair . fromEnt) subs
-                          cpairs <- mapM clean pairs
-                          let upairs = filter (\(x,_,_) -> x == userId) cpairs
-                          return $ map (\(_,x,y) -> (break (== ':') x ,y)) upairs
+--------------------------------------------------------
+--Database Handling
+--------------------------------------------------------
+--functions for retrieving database informaiton and formatting it
     
 synPair (SyntaxCheckSubmission prob time pu)  = (Just pu, prob,time)
 
@@ -150,9 +160,18 @@ transPair (TranslationSubmission prob time pu)  = (Just pu, prob,time)
 
 derPair (DerivationSubmission prob _ time pu) = (Just pu, prob, time)
 
+subsById uid = do synsubs <- yourSubs synPair uid
+                  transsubs <- yourSubs transPair uid
+                  dersubs <- yourSubs derPair uid
+                  return (synsubs, transsubs, dersubs)
+
+yourSubs pair userId = do subs <- runDB $ selectList [] []
+                          let pairs = map (pair . entityVal) subs
+                          cpairs <- mapM clean pairs
+                          let upairs = filter (\(x,_,_) -> x == userId) cpairs
+                          return $ map (\(_,x,y) -> (break (== ':') x ,y)) upairs
+
 clean :: (Maybe (Key User), Text, Text) -> Handler (Text,Text,Text)
 clean (Nothing, s,s')  = return ("annonyous", s,s')
 clean (Just sid, s,s') = do sub <- runDB $ get404 sid
                             return (userIdent sub, s,s')
-
-fromEnt (Entity k v) = v
