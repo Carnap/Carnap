bb0760726886e63f9e36d545f03c30b6e3c7697d
gleachkr <gleachkr@gmail.com>
2017-05-08 12:57:37 -0500

    Rehab tests, preparing for memoization test suite

--- Carnap-Server/Util/Data.hs
+++ Carnap-Server/Util/Data.hs
@@ -6,7 +6,6 @@ data CourseEnrollment = KSUSymbolicI2016 | Birmingham2017
       deriving (Show,Read,Eq)
 derivePersistField "CourseEnrollment"
 
-
 data ProblemSource = CarnapTextbook | BirminghamAssignment2017
       deriving (Show,Read,Eq)
 derivePersistField "ProblemSource"
--- Carnap/test/test.hs
+++ Carnap/test/test.hs
@@ -8,6 +8,8 @@ import Carnap.Core.Unification.FirstOrder
 import Carnap.Core.Unification.ACUI
 import Carnap.Core.Unification.Combination
 import Carnap.Languages.ClassicalSequent.Syntax
+import Carnap.Languages.PurePropositional.Syntax
+import Carnap.Languages.PurePropositional.Logic
 import Carnap.Core.Examples.ACUI
 import System.CPUTime
 import System.Exit (exitFailure)
@@ -17,7 +19,7 @@ main = do putStrLn ""
           altTestPositives psequents
           testNegatives nsequents
           altTestNegatives nsequents
-          timeCombine simpleModusPonens "positive modus ponens" (posTest "combine modus ponens")
+          -- timeCombine simpleModusPonens "positive modus ponens" (posTest "combine modus ponens")
           timeAltCombine simpleCI "alt positive conditional introduction" (posTest "CI 1")
           timeAltCombine simpleCI2 "alt positive conditional introduction 2" (posTest "CI 2")
           timeAltCombine simpleCI3 "alt positive conditional introduction 3" (posTest "CI 3")
@@ -44,7 +46,7 @@ timeAltCombine eqs desc test = do startTime <- getCPUTime
                                   let subs = altCombine eqs
                                   test subs
                                   finishTime <- getCPUTime
-                                  let t = fromIntegral (finishTime - startTime) / 1000000000000
+                                  let t = fromIntegral (finishTime - startTime)
                                   putStrLn $ "Test Results (" ++ desc ++ "):" ++ show t
 
 testTemplate :: Show a => (a -> Bool) -> String -> a -> IO ()
@@ -69,12 +71,12 @@ pacuicase1 = (parseTerm "f(a u b u c u d u e)" :=: parseTerm "f(b u c u d u a)")
 
 nacuicase1 = parseTerm "g(a u b u c u d u e)" :=: parseTerm "f(b u c u d u a)"
 
-pseqcase1 = p :|-: q :=: (p :|-: q)
+pseqcase1 = (p :|-: q) :=: (p :|-: q)
 pseqcase2 = ((p :+: p) :|-: q) :=: (p :|-: q)
-pseqcase3 = p :+: Top :|-: q :=: (p :|-: q)
+pseqcase3 = (p :+: Top :|-: q) :=: (p :|-: q)
 pseqcase4 = ((p :+: GammaV 1) :|-: q) :=: (p :|-: q)
-pseqcase5 = phi :+: GammaV 1 :|-: q :=: (phi :|-: q)
-pseqcase6 = phi :+: phi :|-: q :=: (p :|-: q)
+pseqcase5 = (phi :+: GammaV 1 :|-: q) :=: (phi :|-: q)
+pseqcase6 = (phi :+: phi :|-: q) :=: (p :|-: q)
 psequents = [pseqcase1
             ,pseqcase2
             ,pseqcase3
@@ -85,11 +87,11 @@ psequents = [pseqcase1
             --,pseqcase6 
             ]
 
-nseqcase1 = p' :|-: q :=: (p :|-: q)
-nseqcase2 = p' :+: p :|-: q :=: (p :|-: q)
+nseqcase1 = (p' :|-: q) :=: (p :|-: q)
+nseqcase2 = (p' :+: p :|-: q) :=: (p :|-: q)
 nseqcase3 = ((p' :+: Top) :|-: q) :=: (p :|-: q)
-nseqcase4 = p' :+: Top :|-: q :=: (Top :|-: q)
-nseqcase5 = p :|-: q :=: (p :|-: Bot)
+nseqcase4 = (p' :+: Top :|-: q) :=: (Top :|-: q)
+nseqcase5 = (p :|-: q) :=: (p :|-: Bot)
 nsequents = [nseqcase1
             ,nseqcase2
             ,nseqcase3
@@ -98,50 +100,50 @@ nsequents = [nseqcase1
             ]
 
 
-simpleModusPonens = [ (GammaV 1 :|-: SS (phi_ :->-: phi'_))    :=: (SA p_    :|-: SS (p_ :->-: p'_))
-                    , (GammaV 3 :|-: SS phi_)                  :=: (SA p'_   :|-: SS p_)
-                    , ((GammaV 1 :+: GammaV 3) :|-: SS phi'_)  :=: ((SA p_ :+: SA p'_) :|-: SS p'_)
+simpleModusPonens = [ (GammaV 1 :|-: ss (phi_ :->: phi'_))    :=: (sa p_    :|-: ss (p_ :->: p'_))
+                    , (GammaV 3 :|-: ss phi_)                  :=: (sa p'_   :|-: ss p_)
+                    , ((GammaV 1 :+: GammaV 3) :|-: ss phi'_)  :=: ((sa p_ :+: sa p'_) :|-: ss p'_)
                     ]
 
-simpleModusPonens2 = [ (GammaV 1 :|-: SS (phi_ :->-: phi'_))    :=: (GammaV 2   :|-: SS (p_ :->-: p_))
-                     , (GammaV 3 :|-: SS phi_)                  :=: (GammaV 4   :|-: SS p_)
-                     , ((GammaV 1 :+: GammaV 3) :|-: SS phi'_)  :=: ((GammaV 2 :+: GammaV 4) :|-: SS p_)
+simpleModusPonens2 = [ (GammaV 1 :|-: ss (phi_ :->: phi'_))    :=: (GammaV 2   :|-: ss (p_ :->: p_))
+                     , (GammaV 3 :|-: ss phi_)                  :=: (GammaV 4   :|-: ss p_)
+                     , ((GammaV 1 :+: GammaV 3) :|-: ss phi'_)  :=: ((GammaV 2 :+: GammaV 4) :|-: ss p_)
                      ]
 
-simpleModusPonensErr = [ ((GammaV 1) :|-: SS (phi_ :->-: phi'_)) :=: 
-                         ((SA p_)    :|-: SS (p_ :->-: p'_))
+simpleModusPonensErr = [ ((GammaV 1) :|-: ss (phi_ :->: phi'_)) :=: 
+                         ((sa p_)    :|-: ss (p_ :->: p'_))
                        , ((GammaV 3) :|-: psi) :=: 
-                         ((SA p'_) :|-: q)
-                       , ((SA p_) :+: (SA p'_) :|-: psi') :=: 
+                         ((sa p'_) :|-: q)
+                       , ((sa p_) :+: (sa p'_) :|-: psi') :=: 
                          ((GammaV 2 :+: GammaV 4) :|-: q)
                        ]
 
-simpleCI          = [ ((GammaV 1 :+: SA phi_) :|-: SS phi'_)        :=: (SA p_ :|-: SS p'_)
-                    , (GammaV 1 :|-: SS (phi_ :->-: phi'_))         :=: (Top   :|-: SS (p_ :->-: p'_))
+simpleCI          = [ ((GammaV 1 :+: sa phi_) :|-: ss phi'_)        :=: (sa p_ :|-: ss p'_)
+                    , (GammaV 1 :|-: ss (phi_ :->: phi'_))         :=: (Top   :|-: ss (p_ :->: p'_))
                     ]
 
-simpleCI2         = [ ((GammaV 1 :+: SA phi_) :|-: SS phi'_)        :=: ((SA p_ :+: SA p_) :|-: SS p'_)
-                    , (GammaV 1 :|-: SS (phi_ :->-: phi'_))         :=: (Top               :|-: SS (p_ :->-: p'_))
+simpleCI2         = [ ((GammaV 1 :+: sa phi_) :|-: ss phi'_)        :=: ((sa p_ :+: sa p_) :|-: ss p'_)
+                    , (GammaV 1 :|-: ss (phi_ :->: phi'_))         :=: (Top               :|-: ss (p_ :->: p'_))
                     ]
 
-simpleCI3         = [ ((GammaV 1 :+: SA phi_) :|-: SS phi'_)        :=: ((SA p'_ :+: SA p_) :|-: SS p'_)
-                    , (GammaV 1 :|-: SS (phi_ :->-: phi'_))         :=: (SA p'_             :|-: SS (p_ :->-: p'_))
+simpleCI3         = [ ((GammaV 1 :+: sa phi_) :|-: ss phi'_)        :=: ((sa p'_ :+: sa p_) :|-: ss p'_)
+                    , (GammaV 1 :|-: ss (phi_ :->: phi'_))         :=: (sa p'_             :|-: ss (p_ :->: p'_))
                     ]
 
-simpleCI4         = [ ((GammaV 1 :+: SA phi_) :|-: SS phi'_)        :=: (SA p_ :|-: SS p'_)
-                    , (GammaV 1 :|-: SS (phi_ :->-: phi'_))         :=: (SA p_ :|-: SS (p_ :->-: p'_))
+simpleCI4         = [ ((GammaV 1 :+: sa phi_) :|-: ss phi'_)        :=: (sa p_ :|-: ss p'_)
+                    , (GammaV 1 :|-: ss (phi_ :->: phi'_))         :=: (sa p_ :|-: ss (p_ :->: p'_))
                     ]
 
-simpleCIErr       = [ ((GammaV 1 :+: SA phi_) :|-: SS phi'_)        :=: (SA p'_ :|-: SS p'_)
-                    , (GammaV 1 :|-: SS (phi_ :->-: phi'_))         :=: (Top    :|-: SS (p_ :->-: p'_))
+simpleCIErr       = [ ((GammaV 1 :+: sa phi_) :|-: ss phi'_)        :=: (sa p'_ :|-: ss p'_)
+                    , (GammaV 1 :|-: ss (phi_ :->: phi'_))         :=: (Top    :|-: ss (p_ :->: p'_))
                     ]
 
-simpleCIErr2      = [ ((GammaV 1 :+: SA phi_) :|-: SS phi'_)        :=: (SA p_  :|-: SS p'_)
-                    , (GammaV 1 :|-: SS (phi_ :->-: phi'_))         :=: (SA p'_ :|-: SS (p_ :->-: p'_))
+simpleCIErr2      = [ ((GammaV 1 :+: sa phi_) :|-: ss phi'_)        :=: (sa p_  :|-: ss p'_)
+                    , (GammaV 1 :|-: ss (phi_ :->: phi'_))         :=: (sa p'_ :|-: ss (p_ :->: p'_))
                     ]
 
-simpleCIErr3      = [ ((GammaV 1 :+: SA phi_) :|-: SS phi'_)        :=: (SA p_                     :|-: SS p'_)
-                    , (GammaV 1 :|-: SS (phi_ :->-: phi'_))         :=: ((Top :+: SA p_ :+: SA p'_):|-: SS (p_ :->-: p'_))
+simpleCIErr3      = [ ((GammaV 1 :+: sa phi_) :|-: ss phi'_)        :=: (sa p_                     :|-: ss p'_)
+                    , (GammaV 1 :|-: ss (phi_ :->: phi'_))         :=: ((Top :+: sa p_ :+: sa p'_):|-: ss (p_ :->: p'_))
                     ]
 
 altCombine :: [Equation PropSequentCalc] -> [[Equation PropSequentCalc]]
@@ -159,35 +161,39 @@ succPair ((_ :|-: x):=:(_:|-: y))  = x :=: y
 antPair :: Equation PropSequentCalc -> Equation PropSequentCalc
 antPair  ((x :|-: _):=:(y:|-: _))  = x :=: y
 
-p_ :: PropSequentCalc (Form Bool)
-p_ = SeqProp 1
+p_ :: PureForm
+p_ = PP 1
 
-p'_ :: PropSequentCalc (Form Bool)
-p'_ = SeqProp 2
+p'_ :: PureForm
+p'_ = PP 2
 
-phi_ :: PropSequentCalc (Form Bool)
-phi_ = SeqPhi 1
+phi_ :: PureForm
+phi_ = PPhi 1
 
-phi'_ :: PropSequentCalc (Form Bool)
-phi'_ = SeqPhi 2
+phi'_ :: PureForm
+phi'_ = PPhi 2
+
+ss = SS . liftToSequent 
+
+sa = SA . liftToSequent
 
 p :: PropSequentCalc Antecedent
-p = SA p_
+p = sa p_
 
 p' :: PropSequentCalc Antecedent
-p' = SA p'_
+p' = sa p'_
 
 phi :: PropSequentCalc Antecedent
-phi = SA phi_
+phi = sa phi_
 
 psi :: PropSequentCalc Succedent
-psi = SS phi_
+psi = ss phi_
 
 psi' :: PropSequentCalc Succedent
-psi' = SS phi'_
+psi' = ss phi'_
 
 q :: PropSequentCalc Succedent
-q = SS p_
+q = ss p_
 
 q' :: PropSequentCalc Succedent
-q' = SS p'_
+q' = ss p'_
