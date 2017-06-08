cbc9df5af1e02653dd94fb5a25fed5671e533f9b
gleachkr <gleachkr@gmail.com>
2017-06-08 15:49:36 -0500

    Add JSON Adapter to AllActions

--- Carnap-GHCJS/AllActions/Main.hs
+++ Carnap-GHCJS/AllActions/Main.hs
@@ -4,9 +4,11 @@ import Carnap.GHCJS.Action.SyntaxCheck
 import Carnap.GHCJS.Action.ProofCheck
 import Carnap.GHCJS.Action.Translate
 import Carnap.GHCJS.Action.TruthTable
+import Carnap.GHCJS.Action.AcceptJSON
 
 main :: IO ()
 main = do syntaxCheckAction
           translateAction
           proofCheckAction
           truthTableAction
+          acceptJSONAction
