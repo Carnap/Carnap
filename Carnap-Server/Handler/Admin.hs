8a78005a0bc5b174e345b8df468ee45b188849b7
gleachkr <gleachkr@gmail.com>
2017-12-01 13:31:52 -0600

    Add users overview to admin page

--- Carnap-Server/Handler/Admin.hs
+++ Carnap-Server/Handler/Admin.hs
@@ -11,8 +11,34 @@ import System.FilePath
 import System.Directory (getDirectoryContents,removeFile, doesFileExist)
 
 getAdminR :: Handler Html
-getAdminR = defaultLayout [whamlet|
-                                <div.container>
-                                    <p> Wecome to Admin
-                               |]
+getAdminR = do users <- usersWidget
+               defaultLayout [whamlet|
+                              <div.container>
+                                  <p> Wecome to Admin
+                                  ^{users}
+                             |]
+
+usersWidget :: HandlerT App IO Widget
+usersWidget = do allUserData <- map entityVal <$> (runDB $ selectList [] [])
+                 let allUids = (map userDataUserId  allUserData)
+                 musers <- mapM (\x -> runDB (get x)) allUids
+                 let users = catMaybes musers
+                 let usersAndData = zip users allUserData
+                 return [whamlet|
+                          <div.card style="margin-bottom:20px">
+                              <div.card-header>
+                                  All Users
+                              <div.card-block>
+                                  <table.table.table-striped>
+                                      <thead>
+                                          <th> Identifier
+                                          <th> Name
+                                      <tbody>
+                                          $forall (u,UserData fn ln _ _ _) <- usersAndData
+                                              <tr>
+                                                  <td>
+                                                      <a href=@{UserR (userIdent u)}>#{userIdent u}
+                                                  <td>
+                                                      #{ln}, #{fn}
+                        |]
 
