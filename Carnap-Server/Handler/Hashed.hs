036954387e1f2a426ef9b0eb1d773eccf6b6f0ca
gleachkr <gleachkr@gmail.com>
2017-12-30 11:10:44 -0600

    update hash route to use new datadir settings

--- Carnap-Server/Handler/Hashed.hs
+++ Carnap-Server/Handler/Hashed.hs
@@ -4,6 +4,6 @@ import Import
 import System.Directory (doesDirectoryExist)
 
 getHashedR :: Text -> Handler Html
-getHashedR s = do localbook <- liftIO $ doesDirectoryExist "book"
-                  let path = (if localbook then "book/cache/" else "/root/book/cache/") 
+getHashedR s = do datadir <- appDataRoot <$> (appSettings <$> getYesod)
+                  let path = datadir </> "book/cache/"
                   sendFile typeSvg (path ++ unpack s)
