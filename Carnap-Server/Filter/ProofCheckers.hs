339d2bb18079ad2dfa1d7b27f58329d97050991b
gleachkr <gleachkr@gmail.com>
2018-02-04 15:34:32 -0600

    Expose more options in proofchecker pandoc syntax

--- Carnap-Server/Filter/ProofCheckers.hs
+++ Carnap-Server/Filter/ProofCheckers.hs
@@ -2,54 +2,53 @@ module Filter.ProofCheckers (makeProofChecker,splitIt, intoChunks,formatChunk) w
 
 import Text.Pandoc
 import Data.List.Split (splitOn)
+import Data.Map (unions, fromList, toList)
 import Data.Char (isDigit)
 import Prelude
 
 makeProofChecker :: Block -> Block
-makeProofChecker cb@(CodeBlock (_,classes,_) contents)
-    | "ProofChecker" `elem` classes = Div ("",[],[]) $ map (activate classes) $ intoChunks contents
+makeProofChecker cb@(CodeBlock (_,classes,extra) contents)
+    | "ProofChecker" `elem` classes = Div ("",[],[]) $ map (activate classes extra) $ intoChunks contents
     | "Playground" `elem` classes = Div ("",[],[]) [toPlayground contents]
     | otherwise = cb
 makeProofChecker x = x
 
-activate cls chunk
-    | "Prop"             `elem` cls = actTemplate "data-carnap-system=\"prop\""
-    | "FirstOrder"       `elem` cls = actTemplate "data-carnap-system=\"firstOrder\""
-    | "SecondOrder"      `elem` cls = actTemplate "data-carnap-system=\"secondOrder\""
-    | "PolySecondOrder"  `elem` cls = actTemplate "data-carnap-system=\"polyadicSecondOrder\""
-    | "LogicBook"        `elem` cls = actTemplate "data-carnap-system=\"LogicBook\""
-    | "ForallxSL"        `elem` cls = actTemplate "data-carnap-system=\"magnusSL\" data-carnap-options=\"render\""
-    | "ForallxSLPlus"    `elem` cls = actTemplate "data-carnap-system=\"magnusSLPlus\" data-carnap-options=\"render\""
-    | "ForallxQL"        `elem` cls = actTemplate "data-carnap-system=\"magnusQL\" data-carnap-options=\"render\""
-    | "ZachTFL"          `elem` cls = actTemplate "data-carnap-system=\"thomasBolducAndZachTFL\" data-carnap-options=\"render\""
-    | "ZachFOL"          `elem` cls = actTemplate "data-carnap-system=\"thomasBolducAndZachFOL\" data-carnap-options=\"render\""
-    | "HardegreeSL"      `elem` cls = actTemplate "data-carnap-system=\"hardegreeSL\" data-carnap-options=\"render\""
-    | "HardegreePL"      `elem` cls = actTemplate "data-carnap-system=\"hardegreePL\" data-carnap-options=\"render\""
-    | "HardegreeWTL"     `elem` cls = actTemplate "data-carnap-system=\"hardegreeWTL\" data-carnap-options=\"render guides fonts\""
-    --XXX: Keep render off below until we have nicer rendering of indicies
-    | "HardegreeL"       `elem` cls = actTemplate "data-carnap-system=\"hardegreeL\" data-carnap-options=\"guides fonts\"" 
-    | "HardegreeK"       `elem` cls = actTemplate "data-carnap-system=\"hardegreeK\" data-carnap-options=\"guides fonts\""
-    | "HardegreeT"       `elem` cls = actTemplate "data-carnap-system=\"hardegreeT\" data-carnap-options=\"guides fonts\""
-    | "HardegreeB"       `elem` cls = actTemplate "data-carnap-system=\"hardegreeB\" data-carnap-options=\"guides fonts\""
-    | "HardegreeD"       `elem` cls = actTemplate "data-carnap-system=\"hardegreeD\" data-carnap-options=\"guides fonts\""
-    | "Hardegree4"       `elem` cls = actTemplate "data-carnap-system=\"hardegree4\" data-carnap-options=\"guides fonts\""
-    | "Hardegree5"       `elem` cls = actTemplate "data-carnap-system=\"hardegree5\" data-carnap-options=\"guides fonts\""
-    | "HardegreeMPL"     `elem` cls = actTemplate "data-carnap-system=\"hardegreeMPL\" data-carnap-options=\"guides fonts\""
+activate cls extra chunk
+    | "Prop"             `elem` cls = actTemplate [("system","prop")]
+    | "FirstOrder"       `elem` cls = actTemplate [("system","firstOrder")]
+    | "SecondOrder"      `elem` cls = actTemplate [("system", "secondOrder")]
+    | "PolySecondOrder"  `elem` cls = actTemplate [("system", "polyadicSecondOrder")]
+    | "LogicBook"        `elem` cls = actTemplate [("system", "LogicBook")]
+    | "ForallxSL"        `elem` cls = actTemplate [("system", "magnusSL"), ("options","render")]
+    | "ForallxSLPlus"    `elem` cls = actTemplate [("system", "magnusSLPlus"), ("options","render")]
+    | "ForallxQL"        `elem` cls = actTemplate [("system", "magnusQL"), ("options","render")]
+    | "ZachTFL"          `elem` cls = actTemplate [("system", "thomasBolducAndZachTFL"), ("options","render")]
+    | "ZachFOL"          `elem` cls = actTemplate [("system", "thomasBolducAndZachFOL"), ("options","render")]
+    | "HardegreeSL"      `elem` cls = actTemplate [("system", "hardegreeSL"),  ("options", "render")]
+    | "HardegreePL"      `elem` cls = actTemplate [("system", "hardegreePL"),  ("options", "render")]
+    | "HardegreeWTL"     `elem` cls = actTemplate [("system", "hardegreeWTL"), ("options", "render guides fonts")]
+    | "HardegreeL"       `elem` cls = actTemplate [("system", "hardegreeL"),   ("options", "guides fonts")]
+    | "HardegreeK"       `elem` cls = actTemplate [("system", "hardegreeK"),   ("options", "guides fonts")]
+    | "HardegreeT"       `elem` cls = actTemplate [("system", "hardegreeT"),   ("options", "guides fonts")]
+    | "HardegreeB"       `elem` cls = actTemplate [("system", "hardegreeB"),   ("options", "guides fonts")]
+    | "HardegreeD"       `elem` cls = actTemplate [("system", "hardegreeD"),   ("options", "guides fonts")]
+    | "Hardegree4"       `elem` cls = actTemplate [("system", "hardegree4"),   ("options", "guides fonts")]
+    | "Hardegree5"       `elem` cls = actTemplate [("system", "hardegree5"),   ("options", "guides fonts")]
+    | "HardegreeMPL"     `elem` cls = actTemplate [("system", "hardegreeMPL"), ("options", "guides fonts")]
     | otherwise = RawBlock "html" "<div>No Matching Logic for Derivation</div>"
     where numof = takeWhile (/= ' ')
           seqof = dropWhile (/= ' ')
           (h:t) = formatChunk chunk
+          fixed = [("type","proofchecker"),("goal",seqof h),("submission","saveAs:" ++ numof h)]
           actTemplate opts = RawBlock "html" $ 
                 "<div class=\"exercise\">"
                 ++ "<span> exercise " ++ numof h ++ "</span>"
                 ++ "<div"
-                ++ " data-carnap-type=\"proofchecker\" "
-                ++ opts 
-                ++ " data-carnap-goal=\"" ++ seqof h ++ "\""
-                ++ " data-carnap-submission=\"saveAs:" ++ numof h ++ "\""
+                ++ concatMap (\(x,y) -> " data-carnap-" ++ x ++ "=\"" ++ y ++ "\"") (toList $ unions [fromList extra, fromList opts, fromList fixed])
                 ++ ">"
                 ++ unlines t 
                 ++ "</div></div>"
+
 
 splitIt [] = ([],[])
 splitIt l = case break (== '\n') l of
