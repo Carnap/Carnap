69e5604105a43f02aede42faed1e415a78f12236
gleachkr <gleachkr@gmail.com>
2018-03-16 10:32:56 -0500

    Move svg data to Lib.hs

--- Carnap-GHCJS/src/Carnap/GHCJS/Widget/ProofCheckBox.hs
+++ Carnap-GHCJS/src/Carnap/GHCJS/Widget/ProofCheckBox.hs
@@ -6,8 +6,6 @@ module Carnap.GHCJS.Widget.ProofCheckBox (
     Button(..)) where 
 import Lib
 import Data.IORef (IORef, newIORef,writeIORef,readIORef)
-import Text.Hamlet
-import Text.Blaze.Html.Renderer.String
 import Control.Monad.Trans.State.Lazy
 import Control.Monad.IO.Class
 import Control.Monad (when)
@@ -99,18 +97,6 @@ checkerWith options updateres iog@(IOGoal i o g content _) w = do
                Nothing -> setLinesTo w nd (indentGuides options) [" "]
                (Just iv) -> do setLinesTo w nd (indentGuides options) (altlines iv)
                                if initialUpdate options then updateres w ref iv (g, fd) else return ()
-
-spinnerSVG = renderHtml [shamlet|
-<svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
-    <path opacity="0.2" fill="#000" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946 s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634 c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
-    <path fill="#000" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0 C22.32,8.481,24.301,9.057,26.013,10.047z">
-        <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 20 20" to="360 20 20" dur="0.5s" repeatCount="indefinite"/>|]
-
-checkSVG = renderHtml [shamlet|
-<svg version="1.1" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">
- <g transform="translate(-46.113 -84.018)">
-  <path d="m106.11 84.018a60 60 0 0 0-60 60 60 60 0 0 0 60 60 60 60 0 0 0 60-60 60 60 0 0 0-60-60zm0 12.095a47.905 47.905 0 0 1 47.905 47.905 47.905 47.905 0 0 1-47.905 47.905 47.905 47.905 0 0 1-47.905-47.905 47.905 47.905 0 0 1 47.905-47.905z"/>
-  <path d="m74.95 141.17 23.653 28.743 38.739-45.778"style="fill:none;stroke-width:15;"/>|]
 
 updateLines :: (IsElement e) => Document -> e -> Bool -> EventM HTMLTextAreaElement KeyboardEvent ()
 updateLines w nd hasguides =  do (Just t) <- target :: EventM HTMLTextAreaElement KeyboardEvent (Maybe HTMLTextAreaElement)
--- Carnap-GHCJS/src/Lib.hs
+++ Carnap-GHCJS/src/Lib.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RankNTypes, FlexibleContexts, DeriveDataTypeable, CPP, JavaScriptFFI #-}
+{-# LANGUAGE RankNTypes, QuasiQuotes, FlexibleContexts, DeriveDataTypeable, CPP, JavaScriptFFI #-}
 module Lib
     (genericSendJSON, sendJSON, onEnter, onKey, clearInput,
     getListOfElementsByClass, tryParse, treeToElement, genericTreeToUl,
@@ -7,7 +7,7 @@ module Lib
     loginCheck,errorPopup, genInOutElts, getInOutElts,generateExerciseElts, withLabel,
     formAndLabel,seqAndLabel, folSeqAndLabel, folFormAndLabel,
     message, IOGoal(..), updateWithValue, submissionSource, assignmentKey,
-    initialize,popUpWith) where
+    initialize,popUpWith,spinnerSVG,checkSVG) where
 
 import Data.Aeson
 import Data.Maybe (catMaybes)
@@ -18,6 +18,8 @@ import qualified Data.Map as M
 import Text.Parsec
 import Text.StringLike
 import Text.HTML.TagSoup as TS
+import Text.Hamlet
+import Text.Blaze.Html.Renderer.String
 import Control.Lens
 import Control.Lens.Plated (children)
 import Control.Monad.State
@@ -330,6 +332,22 @@ withLabel parser = do label <- many (digit <|> char '.')
                       spaces
                       s <- parser
                       return (label,s)
+
+------------------
+--1.8 SVG Data  --
+------------------
+
+spinnerSVG = renderHtml [shamlet|
+                        <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve">
+                            <path opacity="0.2" fill="#000" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946 s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634 c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/>
+                                <path fill="#000" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0 C22.32,8.481,24.301,9.057,26.013,10.047z">
+                                        <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 20 20" to="360 20 20" dur="0.5s" repeatCount="indefinite"/>|]
+
+checkSVG = renderHtml [shamlet|
+                      <svg version="1.1" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">
+                       <g transform="translate(-46.113 -84.018)">
+                         <path d="m106.11 84.018a60 60 0 0 0-60 60 60 60 0 0 0 60 60 60 60 0 0 0 60-60 60 60 0 0 0-60-60zm0 12.095a47.905 47.905 0 0 1 47.905 47.905 47.905 47.905 0 0 1-47.905 47.905 47.905 47.905 0 0 1-47.905-47.905 47.905 47.905 0 0 1 47.905-47.905z"/>
+                           <path d="m74.95 141.17 23.653 28.743 38.739-45.778"style="fill:none;stroke-width:15;"/>|]
 
 --------------------------------------------------------
 --2. FFI Wrappers
