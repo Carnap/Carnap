5b8a5090e2a377e2f4cbf5e4c4b28c338366386c
gleachkr <gleachkr@gmail.com>
2018-04-09 15:17:10 -0500

    Update forallx notation for 2018 version

--- Carnap/src/Carnap/Languages/PurePropositional/Logic/ThomasBolducAndZach.hs
+++ Carnap/src/Carnap/Languages/PurePropositional/Logic/ThomasBolducAndZach.hs
@@ -29,9 +29,8 @@ data ThomasBolducAndZachTFL = ConjIntro
                             | BicoIntro3 | BicoIntro4
                             | BicoElim1  | BicoElim2
                             | NegeIntro1 | NegeIntro2
-                            | Tertium1   | Tertium2
-                            | Tertium3   | Tertium4
-                            | ContIntro  | ContElim 
+                            | NegeElim   | ContElim 
+                            | Indirect1  | Indirect2
                             | As         | Pr
                             deriving (Eq)
                             --skipping derived rules for now
@@ -40,13 +39,15 @@ instance Show ThomasBolducAndZachTFL where
         show ConjIntro  = "∧I"
         show ConjElim1  = "∧E"
         show ConjElim2  = "∧E"
+        show NegeIntro1 = "¬I"
+        show NegeIntro2 = "¬I"
+        show NegeElim   = "¬I"
+        show Indirect1  = "IP"
+        show Indirect2  = "IP"
         show CondIntro1 = "→I"
         show CondIntro2 = "→I"
         show CondElim   = "→E"
-        show NegeIntro1 = "¬I"
-        show NegeIntro2 = "¬I"
-        show ContIntro  = "⊥I"
-        show ContElim   = "⊥E"
+        show ContElim   = "X"
         show DisjElim1  = "∨E"
         show DisjElim2  = "∨E"
         show DisjElim3  = "∨E"
@@ -61,88 +62,79 @@ instance Show ThomasBolducAndZachTFL where
         show BicoElim2  = "↔E"
         show As         = "AS"
         show Pr         = "PR"
-        show Tertium1   = "TND"
-        show Tertium2   = "TND"
-        show Tertium3   = "TND"
-        show Tertium4   = "TND"
 
 instance Inference ThomasBolducAndZachTFL PurePropLexicon (Form Bool) where
         ruleOf ConjIntro  = adjunction
         ruleOf ConjElim1  = simplificationVariations !! 0
         ruleOf ConjElim2  = simplificationVariations !! 1
+        ruleOf NegeIntro1 = explicitConstructiveFalsumReductioVariations !! 0
+        ruleOf NegeIntro2 = explicitConstructiveFalsumReductioVariations !! 1
+        ruleOf NegeElim   = falsumIntroduction
+        ruleOf Indirect1  = explicitNonConstructiveFalsumReductioVariations !! 0
+        ruleOf Indirect2  = explicitNonConstructiveFalsumReductioVariations !! 1
+        ruleOf CondIntro1 = conditionalProofVariations !! 0
+        ruleOf CondIntro2 = conditionalProofVariations !! 1
+        ruleOf ContElim   = falsumElimination
         ruleOf DisjIntro1 = additionVariations !! 0
         ruleOf DisjIntro2 = additionVariations !! 1 
         ruleOf DisjElim1  = proofByCasesVariations !! 0
         ruleOf DisjElim2  = proofByCasesVariations !! 1
         ruleOf DisjElim3  = proofByCasesVariations !! 2
         ruleOf DisjElim4  = proofByCasesVariations !! 3
-        ruleOf CondIntro1 = conditionalProofVariations !! 0
-        ruleOf CondIntro2 = conditionalProofVariations !! 1
-        ruleOf CondElim   = modusPonens
         ruleOf BicoIntro1 = biconditionalProofVariations !! 0
         ruleOf BicoIntro2 = biconditionalProofVariations !! 1
         ruleOf BicoIntro3 = biconditionalProofVariations !! 2
         ruleOf BicoIntro4 = biconditionalProofVariations !! 3
         ruleOf BicoElim1  = biconditionalPonensVariations !! 0
         ruleOf BicoElim2  = biconditionalPonensVariations !! 1
-        ruleOf NegeIntro1 = explicitConstructiveFalsumReductioVariations !! 0
-        ruleOf NegeIntro2 = explicitConstructiveFalsumReductioVariations !! 1
-        ruleOf ContIntro  = falsumIntroduction
-        ruleOf ContElim   = falsumElimination
-        ruleOf Tertium1   = tertiumNonDaturVariations !! 0
-        ruleOf Tertium2   = tertiumNonDaturVariations !! 1
-        ruleOf Tertium3   = tertiumNonDaturVariations !! 2
-        ruleOf Tertium4   = tertiumNonDaturVariations !! 3
         ruleOf As         = axiom
         ruleOf Pr         = axiom
 
         indirectInference x
             | x `elem` [ DisjElim1, DisjElim2, DisjElim3, DisjElim4
-                       , CondIntro1, CondIntro2
-                       , BicoIntro1, BicoIntro2 , BicoIntro3, BicoIntro4
-                       ] = Just PolyProof
-            | x `elem` [Tertium1,Tertium2,Tertium3,Tertium4] = Just (PolyTypedProof 2 (ProofType 1 1))
-            | x `elem` [NegeIntro1, NegeIntro2] = Just assumptiveProof
+                       , CondIntro1, CondIntro2 , BicoIntro1, BicoIntro2
+                       , BicoIntro3, BicoIntro4] = Just PolyProof
+            | x `elem` [ NegeIntro1, NegeIntro2
+                       , Indirect1, Indirect2] = Just assumptiveProof
             | otherwise = Nothing
 
         isAssumption As = True
         isAssumption _  = False
 
 parseThomasBolducAndZachTFL :: RuntimeNaturalDeductionConfig PurePropLexicon (Form Bool) -> Parsec String u [ThomasBolducAndZachTFL]
-parseThomasBolducAndZachTFL ders = do r <- choice (map (try . string) [ "AS","PR","&I","/\\I", "∧I","&E","/\\E","∧E","CI","->I","→I","→E","CE","->E", "→E"
-                                                                      , "~I","-I", "¬I","!?I","!?O", "vI","\\/I","∨I", "vE","\\/E", "∨E","BI","<->I", "↔I", "BE", "<->E"
-                                                                      , "↔E", "TND"])
+parseThomasBolducAndZachTFL ders = do r <- choice (map (try . string) [ "AS","PR","&I","/\\I", "∧I","&E","/\\E","∧E", "~I","-I", "¬I","IP","->I","→I","→E","->E", "→E"
+                                                                      ,"X", "vI","\\/I","∨I", "vE","\\/E", "∨E","<->I", "↔I","<->E" , "↔E"])
                                       case r of
                                            "AS"   -> return [As]
                                            "PR"   -> return [Pr]
                                            "&I"   -> return [ConjIntro]
-                                           "&E"   -> return [ConjElim1, ConjElim2]
                                            "/\\I" -> return [ConjIntro]
-                                           "/\\E" -> return [ConjElim1, ConjElim2]
                                            "∧I"   -> return [ConjIntro]
+                                           "&E"   -> return [ConjElim1, ConjElim2]
+                                           "/\\E" -> return [ConjElim1, ConjElim2]
                                            "∧E"   -> return [ConjElim1, ConjElim2]
-                                           "CI"   -> return [CondIntro1,CondIntro2]
-                                           "CE"   -> return [CondElim]
-                                           "->I"  -> return [CondIntro1,CondIntro2]
-                                           "->E"  -> return [CondElim]
-                                           "→I"   -> return [CondIntro1,CondIntro2]
-                                           "→E"   -> return [CondElim]
                                            "~I"   -> return [NegeIntro1, NegeIntro2]
                                            "¬I"   -> return [NegeIntro1, NegeIntro2]
                                            "-I"   -> return [NegeIntro1, NegeIntro2]
-                                           "!?I"  -> return [ContIntro]
-                                           "!?E"  -> return [ContElim]
+                                           "~E"   -> return [NegeElim]
+                                           "¬E"   -> return [NegeElim]
+                                           "-E"   -> return [NegeElim]
+                                           "IP"   -> return [Indirect1, Indirect2]
+                                           "->I"  -> return [CondIntro1,CondIntro2]
+                                           "→I"   -> return [CondIntro1,CondIntro2]
+                                           "->E"  -> return [CondElim]
+                                           "→E"   -> return [CondElim]
+                                           "X"    -> return [ContElim]
+                                           "∨I"   -> return [DisjIntro1, DisjIntro2]
                                            "vI"   -> return [DisjIntro1, DisjIntro2]
-                                           "vE"   -> return [DisjElim1, DisjElim2, DisjElim3, DisjElim4]
                                            "\\/I" -> return [DisjIntro1, DisjIntro2]
+                                           "∨E"   -> return [DisjElim1, DisjElim2, DisjElim3, DisjElim4]
+                                           "vE"   -> return [DisjElim1, DisjElim2, DisjElim3, DisjElim4]
                                            "\\/E" -> return [DisjElim1, DisjElim2, DisjElim3, DisjElim4]
-                                           "BI"   -> return [BicoIntro1, BicoIntro2, BicoIntro3, BicoIntro4]
-                                           "BE"   -> return [BicoElim1, BicoElim2]
                                            "<->I" -> return [BicoIntro1, BicoIntro2, BicoIntro3, BicoIntro4]
-                                           "<->E" -> return [BicoElim1, BicoElim2]
                                            "↔I"   -> return [BicoIntro1, BicoIntro2, BicoIntro3, BicoIntro4]
+                                           "<->E" -> return [BicoElim1, BicoElim2]
                                            "↔E"   -> return [BicoElim1, BicoElim2]
-                                           "TND"  -> return [Tertium1, Tertium2, Tertium3, Tertium4]
 
 parseThomasBolducAndZachTFLProof :: RuntimeNaturalDeductionConfig PurePropLexicon (Form Bool) -> String -> [DeductionLine ThomasBolducAndZachTFL PurePropLexicon (Form Bool)]
 parseThomasBolducAndZachTFLProof ders = toDeductionFitch (parseThomasBolducAndZachTFL ders) (purePropFormulaParser $ extendedLetters {hasBooleanConstants = True})
