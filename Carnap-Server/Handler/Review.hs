947858059e936a4c35304fdbead79ccffe40d927
gleachkr <gleachkr@gmail.com>
2018-07-04 09:55:43 -0500

    Get extra credit update working.

--- Carnap-Server/Handler/Review.hs
+++ Carnap-Server/Handler/Review.hs
@@ -1,4 +1,4 @@
-module Handler.Review (getReviewR) where
+module Handler.Review (getReviewR, putReviewR) where
 
 import Import
 import Util.Database
@@ -9,10 +9,10 @@ import Carnap.GHCJS.SharedTypes
 putReviewR :: Text -> Handler Value
 putReviewR filename =
         do (Entity key val, _) <- getAssignmentByFilename filename
-           ((theUpdate,_),_) <- runFormPost (identifyForm "updateSubmission" $ updateSubmissionForm)
+           ((theUpdate,_),_) <- runFormPost (identifyForm "updateSubmission" $ updateSubmissionForm Nothing "" "")
            case theUpdate of
                FormSuccess (ident, serializeduid, extra) -> do
-                   success <- runDB $ do case readMaybe (unpack serializeduid) of 
+                   success <- runDB $ do case readMaybe serializeduid of 
                                                Just uid -> do msub <- getBy (UniqueProblemSubmission ident uid (Assignment (show key)))
                                                               case msub of 
                                                                    Just (Entity k _) -> update k [ProblemSubmissionExtra =. Just extra] >> return True
@@ -26,7 +26,6 @@ getReviewR :: Text -> Handler Html
 getReviewR filename = 
         do (Entity key val, _) <- getAssignmentByFilename filename
            unsortedProblems <- runDB $ selectList [ProblemSubmissionAssignmentId ==. Just key] []
-           (updateSubmissionWidget,enctypeUpdateSubmission) <- generateFormPost (identifyForm "updateSubmission" $ updateSubmissionForm)
            let problems = sortBy theSorting unsortedProblems
            defaultLayout $ do
                addScript $ StaticR js_popper_min_js
@@ -34,19 +33,7 @@ getReviewR filename =
                addScript $ StaticR ghcjs_allactions_lib_js
                addScript $ StaticR ghcjs_allactions_out_js
                addStylesheet $ StaticR css_exercises_css
-               [whamlet|
-               <div.container>
-                   <article>
-                       $if null problems
-                            <p> Nothing to review!
-                       $else
-                            $forall p <- problems
-                                ^{renderProblem p}
-                                <form enctype=#{enctypeUpdateSubmission}>
-                                    ^{updateSubmissionWidget}
-                                    <div.form-group>
-                                        <input.btn.btn-primary type=submit value="update">
-               |]
+               $(widgetFile "review")
                addScript $ StaticR ghcjs_allactions_runmain_js
     where theSorting p p' = scompare s s'
               where s = unpack . problemSubmissionIdent . entityVal $ p
@@ -60,32 +47,44 @@ getReviewR filename =
                                                        | otherwise -> scompare (drop 1 t) (drop 1 t')
 
 renderProblem (Entity key val) = do
+        let ident = problemSubmissionIdent val
+            uid = problemSubmissionUserId val
+            extra = problemSubmissionExtra val
+        (updateSubmissionWidget,enctypeUpdateSubmission) <- generateFormPost (identifyForm "updateSubmission" $ updateSubmissionForm extra ident (show uid))
         case (problemSubmissionType val, problemSubmissionData val) of
             (Derivation, DerivationData content der) ->
                 [whamlet|
-                    <h4>#{problemSubmissionIdent val}
+                    <h4>#{ident}
                     <div>#{content}
                     <pre>#{der}
+                    <form.updateSubmission enctype=#{enctypeUpdateSubmission}>
+                        ^{updateSubmissionWidget}
+                        <div.form-group>
+                            <input.btn.btn-primary type=submit value="update">
                 |]
             (TruthTable, TruthTableData content tt) -> 
                 [whamlet|
-                    <h4>#{problemSubmissionIdent val}
+                    <h4>#{ident}
                     <div>#{content}
                     <pre>#{show tt}
+                    <form.updateSubmission enctype=#{enctypeUpdateSubmission}>
+                        ^{updateSubmissionWidget}
+                        <div.form-group>
+                            <input.btn.btn-primary type=submit value="update">
                 |]
             (Translation, TranslationData content trans) -> 
                 [whamlet|
-                    <h4>#{problemSubmissionIdent val}
+                    <h4>#{ident}
                     <div>#{content}
                     <pre>#{trans}
+                    <form.updateSubmission enctype=#{enctypeUpdateSubmission}>
+                        ^{updateSubmissionWidget}
+                        <div.form-group>
+                            <input.btn.btn-primary type=submit value="update">
                 |]
             _ -> return ()
 
-updateSubmissionForm = renderBootstrap3 BootstrapBasicForm $ (,,)
-            <$> areq identField "" Nothing
-            <*> areq uidField "" Nothing
-            <*> areq intField (bfs ("Extra Credit Points"::Text)) Nothing
-    where identField :: (Monad m, RenderMessage (HandlerSite m) FormMessage) => Field m Text 
-          identField = hiddenField
-          uidField :: (Monad m, RenderMessage (HandlerSite m) FormMessage) => Field m Text
-          uidField = hiddenField
+updateSubmissionForm extra ident uid = renderBootstrap3 BootstrapBasicForm $ (,,)
+            <$> areq hiddenField "" (Just ident)
+            <*> areq hiddenField "" (Just uid) 
+            <*> areq intField (bfs ("Extra Credit Points"::Text)) extra
