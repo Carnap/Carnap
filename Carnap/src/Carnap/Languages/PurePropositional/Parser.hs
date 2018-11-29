2d7857ed47926f02d3166effcca1370343f6683d
gleachkr <gleachkr@gmail.com>
2018-11-29 07:36:56 -0600

    Move opTable to parser options

--- Carnap/src/Carnap/Languages/PurePropositional/Parser.hs
+++ Carnap/src/Carnap/Languages/PurePropositional/Parser.hs
@@ -9,24 +9,27 @@ import Text.Parsec.Expr
 data PurePropositionalParserOptions u m = PurePropositionalParserOptions 
                                         { atomicSentenceParser :: ParsecT String u m PureForm 
                                         , hasBooleanConstants :: Bool
+                                        , opTable :: [[Operator String u m PureForm]]
                                         }
 
 standardLetters :: Monad m => PurePropositionalParserOptions u m
 standardLetters = PurePropositionalParserOptions 
                         { atomicSentenceParser = sentenceLetterParser "PQRSTUVW" 
                         , hasBooleanConstants = False
+                        , opTable = standardOpTable
                         }
 
 extendedLetters :: Monad m => PurePropositionalParserOptions u m
 extendedLetters = PurePropositionalParserOptions 
                         { atomicSentenceParser = sentenceLetterParser "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
                         , hasBooleanConstants = False
+                        , opTable = standardOpTable
                         }
 
 --this parses as much formula as it can, but is happy to return an output if the
 --initial segment of a string is a formula
 purePropFormulaParser :: Monad m => PurePropositionalParserOptions u m -> ParsecT String u m PureForm
-purePropFormulaParser opts = buildExpressionParser opTable subFormulaParser
+purePropFormulaParser opts = buildExpressionParser (opTable opts) subFormulaParser
     --subformulas are either
     where subFormulaParser = (parenParser (purePropFormulaParser opts) <* spaces)  --formulas wrapped in parentheses
                           <|> unaryOpParser [parseNeg] subFormulaParser --negations or modalizations of subformulas
@@ -34,7 +37,8 @@ purePropFormulaParser opts = buildExpressionParser opTable subFormulaParser
                           <|> if hasBooleanConstants opts then try (booleanConstParser <* spaces) else parserZero
                           <|> ((schemevarParser <* spaces) <?> "")
 
-opTable :: Monad m => [[Operator String u m PureForm]]
-opTable = [[ Prefix (try parseNeg)], 
-          [Infix (try parseOr) AssocLeft, Infix (try parseAnd) AssocLeft],
-          [Infix (try parseIf) AssocNone, Infix (try parseIff) AssocNone]]
+standardOpTable :: Monad m => [[Operator String u m PureForm]]
+standardOpTable = [ [ Prefix (try parseNeg)]
+                  , [Infix (try parseOr) AssocLeft, Infix (try parseAnd) AssocLeft]
+                  , [Infix (try parseIf) AssocNone, Infix (try parseIff) AssocNone]
+                  ]
