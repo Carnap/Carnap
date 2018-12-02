e4972ada704c1c86eeaab976a56cf3ba299320b0
gleachkr <gleachkr@gmail.com>
2018-12-02 16:02:49 -0600

    Expose hausmanPL

--- Carnap-GHCJS/src/Carnap/GHCJS/Action/ProofCheck.hs
+++ Carnap-GHCJS/src/Carnap/GHCJS/Action/ProofCheck.hs
@@ -17,7 +17,7 @@ import Carnap.Languages.PureFirstOrder.Logic as FOL
     ( DerivedRule(..), folCalc, montagueQCCalc, magnusQLCalc , thomasBolducAndZachFOLCalc
     , hardegreePLCalc , goldfarbNDCalc, goldfarbAltNDCalc
     , goldfarbNDPlusCalc, goldfarbAltNDPlusCalc , logicBookPDPlusCalc
-    , logicBookPDCalc) 
+    , logicBookPDCalc, hausmanPLCalc) 
 import Carnap.Languages.ModalPropositional.Logic as MPL
     ( hardegreeWTLCalc, hardegreeLCalc, hardegreeKCalc, hardegreeTCalc
     , hardegreeBCalc, hardegreeDCalc, hardegreeFourCalc, hardegreeFiveCalc)
@@ -118,6 +118,7 @@ activateChecker drs w (Just iog@(IOGoal i o g _ opts)) -- TODO: need to update n
         | sys == "LogicBookPD"               = tryParse logicBookPDCalc folChecker
         | sys == "LogicBookPDPlus"           = tryParse logicBookPDPlusCalc folChecker
         | sys == "hausmanSL"                 = tryParse hausmanSLCalc propChecker
+        | sys == "hausmanPL"                 = tryParse hausmanPLCalc folChecker
         | sys == "magnusSL"                  = tryParse magnusSLCalc propChecker
         | sys == "magnusSLPlus"              = tryParse magnusSLPlusCalc propChecker
         | sys == "magnusQL"                  = tryParse magnusQLCalc folChecker
