8c8bb82c472208181dfb83db75a96868524d86cf
gleachkr <gleachkr@gmail.com>
2019-06-24 11:50:53 -0500

    Extract target formulas from rules

--- Carnap/src/Carnap/Calculi/Tableau/Checker.hs
+++ Carnap/src/Carnap/Calculi/Tableau/Checker.hs
@@ -12,6 +12,7 @@ import Data.Tree
 import Data.List
 import Data.Typeable
 import Control.Monad.State
+import Control.Lens
 
 --This function should swap out the contents of each node in a tableau for
 --appropriate feedback, or an indication that the node is correct.
@@ -42,20 +43,34 @@ validateNode n ns = case hosolve [seqTarget n :=: seqRuleTarget n] of
                         Left (NoUnify _ _) -> return $ Feedback "Rule applied incorrectly to node" --TODO Improve feedback here
                         Right subs -> do sub <- subs
                                          childSeqs <- permutations (map tableauNodeSeq ns)
-                                         let subbedChildren = map (applySub sub) (ruleChildren theRule)
-                                             subbedParent = applySub sub (ruleParent theRule)
+                                         let subbedChildren = map (applySub sub) (upperSequents theRule)
+                                             subbedParent = applySub sub (lowerSequent theRule)
                                              prob = (subbedParent :=: tableauNodeSeq n) : zipWith (:=:) subbedChildren childSeqs
                                          case acuisolve prob of
                                              Left (NoUnify _ _) -> return $ Feedback "Rule applied incorrectly to branch" --TODO Improve feedback here.
                                              Right subs -> return Correct
-    where theRule = tableauNodeRule n
+    where theRule = schemeOf . tableauNodeRule $ n
           seqTarget :: Sequentable lex => TableauNode lex sem rule -> ClassicalSequentOver lex sem
           seqTarget = liftToSequent . tableauNodeTarget
-          seqRuleTarget :: Sequentable lex => TableauNode lex sem rule -> ClassicalSequentOver lex sem
-          seqRuleTarget = liftToSequent . ruleTarget . tableauNodeRule
+          seqRuleTarget :: 
+            ( Sequentable lex
+            , PrismSubstitutionalVariable lex
+            , FirstOrder (ClassicalSequentOver lex)
+            , Typeable sem
+            ) => TableauNode lex sem rule -> ClassicalSequentOver lex sem
+          seqRuleTarget = liftToSequent . schemeTarget . schemeOf . tableauNodeRule
 
-ruleTarget = undefined
+schemeTarget :: 
+    ( FirstOrder (ClassicalSequentOver lex)
+    , Typeable sem
+    , Sequentable lex
+    , PrismSubstitutionalVariable lex
+    ) => SequentRule lex sem -> FixLang lex sem
+schemeTarget r = case filter isVar . toListOf concretes . lowerSequent $ r of
+                 [f] -> fromSequent f
+                 _ -> error $ "rule lacks a unique target"
 
-ruleParent = undefined
+schemeOf = undefined
 
 ruleChildren = undefined
+
