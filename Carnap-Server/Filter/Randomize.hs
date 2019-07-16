56ead149d1ace0458ed8ba36701560e3a6f2e1e4
gleachkr <gleachkr@gmail.com>
2019-07-16 11:21:23 -0500

    Allow randomization from problem sets

---
+++ Carnap-Server/Filter/Randomize.hs
@@ -0,0 +1,19 @@
+module Filter.Randomize (randomizeProblems) where
+
+import Data.List
+import Data.Hashable
+import Text.Pandoc
+import System.Random
+import Prelude
+
+randomizeProblems :: Int -> Block -> Block
+randomizeProblems salt (Div ("",[],[]) contents) = Div ("",[],[]) (randomize salt contents)
+randomizeProblems _ x = x
+
+randomize :: Int -> [Block] -> [Block]
+randomize salt = map pickOne . groupBy sameEx
+    where sameEx a b | Just x <- exercise a, Just y <- exercise b, x == y = True
+                     | otherwise = False
+          exercise (Div ("",["exercise"],[]) (Plain [Span ("",[],[]) [Str h]]:_)) = Just h
+          exercise _ = Nothing
+          pickOne l =  l !! (fst $ randomR (0,length l - 1) (mkStdGen (hashWithSalt salt (exercise $ head l))))
