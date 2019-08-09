821c23f0326d848b04bc473a70bc2be6be9941db
gleachkr <gleachkr@gmail.com>
2019-08-09 14:24:33 -0500

    A little more robustness

--- Carnap-Server/Filter/Util.hs
+++ Carnap-Server/Filter/Util.hs
@@ -15,7 +15,9 @@ splitIt l = case break (== '\n') l of
                 y -> y
 
 intoChunks [] = []
-intoChunks l = let (h,t) = splitIt l in h : intoChunks t
+intoChunks l = case splitIt l of 
+                 ([],t) -> intoChunks t
+                 (h,t) -> h : intoChunks t
 
 formatChunk = map cleanProof . lines
     where cleanProof ('|':xs) = dropWhile (\y -> isDigit y || (y == '.')) xs
