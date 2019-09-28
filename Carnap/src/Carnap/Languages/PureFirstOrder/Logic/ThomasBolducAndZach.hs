88683a78713b37a2167ca26bf42b4f353a0e52c7
gleachkr <gleachkr@gmail.com>
2019-09-28 14:11:29 -0500

    Separate Calgary FOL and FOLCore

--- Carnap/src/Carnap/Languages/PureFirstOrder/Logic/ThomasBolducAndZach.hs
+++ Carnap/src/Carnap/Languages/PureFirstOrder/Logic/ThomasBolducAndZach.hs
@@ -23,12 +23,14 @@ import Carnap.Languages.PurePropositional.Logic.Rules (premConstraint,axiom)
 -- A system of first-order logic resembling system FOL from the Calcary
 -- Remix of forall x
 
-data ThomasBolducAndZachFOL = ThomasBolducAndZachTFL P.ThomasBolducAndZachTFL | UI | UE | EI | EE1 | EE2 | IDI | IDE1 | IDE2 | QN1 | QN2 | QN3 | QN4
+data ThomasBolducAndZachFOLCore = TFLC P.ThomasBolducAndZachTFLCore | UI | UE | EI | EE1 | EE2 | IDI | IDE1 | IDE2 
                             | Pr (Maybe [(ClassicalSequentOver PureLexiconFOL (Sequent (Form Bool)))])
                     deriving Eq
 
-instance Show ThomasBolducAndZachFOL where
-        show (ThomasBolducAndZachTFL x) = show x
+data ThomasBolducAndZachFOL = TFL P.ThomasBolducAndZachTFL | FOL ThomasBolducAndZachFOLCore | QN1 | QN2 | QN3 | QN4
+
+instance Show ThomasBolducAndZachFOLCore where
+        show (TFLC x) = show x
         show UI          = "∀I"
         show UE          = "∀E"
         show EI          = "∃I"
@@ -37,37 +39,36 @@ instance Show ThomasBolducAndZachFOL where
         show IDI         = "=I"
         show IDE1        = "=E"
         show IDE2        = "=E"
+        show (Pr _)      = "PR"
+
+instance Show ThomasBolducAndZachFOL where
+        show (TFL x)     = show x
+        show (FOL x)     = show x
         show QN1         = "CQ"
         show QN2         = "CQ"
         show QN3         = "CQ"
         show QN4         = "CQ"
-        show (Pr _)      = "PR"
 
-instance Inference ThomasBolducAndZachFOL PureLexiconFOL (Form Bool) where
+instance Inference ThomasBolducAndZachFOLCore PureLexiconFOL (Form Bool) where
 
+         ruleOf r@(TFLC _) = premisesOf r ∴ conclusionOf r
          ruleOf UI     = universalGeneralization
          ruleOf UE     = universalInstantiation
          ruleOf EI     = existentialGeneralization
          ruleOf EE1    = existentialDerivation !! 0 
          ruleOf EE2    = existentialDerivation !! 1 
          ruleOf IDI    = eqReflexivity
-         ruleOf QN1    = quantifierNegation !! 0 
-         ruleOf QN2    = quantifierNegation !! 1
-         ruleOf QN3    = quantifierNegation !! 2
-         ruleOf QN4    = quantifierNegation !! 3
          ruleOf (Pr _) = axiom
-         ruleOf r@(ThomasBolducAndZachTFL _) = premisesOf r ∴ conclusionOf r
-
          ruleOf IDE1  = leibnizLawVariations !! 0
          ruleOf IDE2  = leibnizLawVariations !! 1
 
-         premisesOf (ThomasBolducAndZachTFL x) = map liftSequent (premisesOf x)
+         premisesOf (TFLC x) = map liftSequent (premisesOf x)
          premisesOf r = upperSequents (ruleOf r)
          
-         conclusionOf (ThomasBolducAndZachTFL x) = liftSequent (conclusionOf x)
+         conclusionOf (TFLC x) = liftSequent (conclusionOf x)
          conclusionOf r = lowerSequent (ruleOf r)
 
-         indirectInference (ThomasBolducAndZachTFL x) = indirectInference x
+         indirectInference (TFLC x) = indirectInference x
          indirectInference x  
             | x `elem` [ EE1,EE2 ] = Just assumptiveProof
             | otherwise = Nothing
@@ -78,15 +79,43 @@ instance Inference ThomasBolducAndZachFOL PureLexiconFOL (Form Bool) where
          restriction (Pr prems) = Just (premConstraint prems)
          restriction _     = Nothing
 
-         isAssumption (ThomasBolducAndZachTFL x) = isAssumption x
+         isAssumption (TFLC x) = isAssumption x
          isAssumption _ = False
 
          isPremise (Pr _) = True
          isPremise _ = False
 
-parseThomasBolducAndZachFOL rtc = try quantRule <|> (map ThomasBolducAndZachTFL <$> parseProp)
-    where parseProp = P.parseThomasBolducAndZachTFL (RuntimeNaturalDeductionConfig mempty mempty)
-          quantRule = do r <- choice (map (try . string) ["∀I", "AI", "∀E", "AE", "∃I", "EI", "∃E", "EE", "=I","=E","CQ","PR"])
+instance Inference ThomasBolducAndZachFOL PureLexiconFOL (Form Bool) where
+
+         ruleOf (FOL x) = ruleOf x
+         ruleOf r@(TFL _) = premisesOf r ∴ conclusionOf r
+         ruleOf QN1    = quantifierNegation !! 0 
+         ruleOf QN2    = quantifierNegation !! 1
+         ruleOf QN3    = quantifierNegation !! 2
+         ruleOf QN4    = quantifierNegation !! 3
+
+         premisesOf (TFL x) = map liftSequent (premisesOf x)
+         premisesOf r = upperSequents (ruleOf r)
+         
+         conclusionOf (TFL x) = liftSequent (conclusionOf x)
+         conclusionOf r = lowerSequent (ruleOf r)
+
+         indirectInference (TFL x) = indirectInference x
+         indirectInference (FOL x) = indirectInference x
+         indirectInference _ = Nothing
+
+         restriction (FOL x) = restriction x
+         restriction _       = Nothing
+
+         isAssumption (TFL x) = isAssumption x
+         isAssumption _ = False
+
+         isPremise (FOL x) = isPremise x
+         isPremise _ = False
+
+parseThomasBolducAndZachFOLCore rtc = try quantRule <|> (map TFLC <$> parseProp)
+    where parseProp = P.parseThomasBolducAndZachTFLCore (RuntimeNaturalDeductionConfig mempty mempty)
+          quantRule = do r <- choice (map (try . string) ["∀I", "AI", "∀E", "AE", "∃I", "EI", "∃E", "EE", "=I","=E","PR"])
                          case r of 
                             r | r `elem` ["∀I","AI"] -> return [UI]
                               | r `elem` ["∀E","AE"] -> return [UE]
@@ -94,8 +123,15 @@ parseThomasBolducAndZachFOL rtc = try quantRule <|> (map ThomasBolducAndZachTFL 
                               | r `elem` ["∃E","EE"] -> return [EE1, EE2]
                               | r == "=I" -> return [IDI]
                               | r == "=E" -> return [IDE1,IDE2]
-                              | r == "CQ" -> return [QN1,QN2,QN3,QN4]
                               | r == "PR" -> return [Pr (problemPremises rtc)]
+
+parseThomasBolducAndZachFOL rtc = try (map TFL <$> parseProp) <|> try (map FOL <$> quantRule) <|> try cqRule
+    where parseProp = P.parseThomasBolducAndZachTFL (RuntimeNaturalDeductionConfig mempty mempty)
+          quantRule = parseThomasBolducAndZachFOLCore rtc
+          cqRule = string "CQ" >> return [QN1,QN2,QN3,QN4]
+
+parseThomasBolducAndZachFOLCoreProof :: RuntimeNaturalDeductionConfig PureLexiconFOL (Form Bool) -> String -> [DeductionLine ThomasBolducAndZachFOLCore PureLexiconFOL (Form Bool)]
+parseThomasBolducAndZachFOLCoreProof ders = toDeductionFitch (parseThomasBolducAndZachFOLCore ders) thomasBolducAndZachFOLFormulaParser
 
 parseThomasBolducAndZachFOLProof :: RuntimeNaturalDeductionConfig PureLexiconFOL (Form Bool) -> String -> [DeductionLine ThomasBolducAndZachFOL PureLexiconFOL (Form Bool)]
 parseThomasBolducAndZachFOLProof ders = toDeductionFitch (parseThomasBolducAndZachFOL ders) thomasBolducAndZachFOLFormulaParser
@@ -103,6 +139,16 @@ parseThomasBolducAndZachFOLProof ders = toDeductionFitch (parseThomasBolducAndZa
 thomasBolducAndZachFOLCalc = mkNDCalc
     { ndRenderer = FitchStyle StandardFitch
     , ndParseProof = parseThomasBolducAndZachFOLProof
+    , ndProcessLine = hoProcessLineFitch
+    , ndProcessLineMemo = Just hoProcessLineFitchMemo
+    , ndParseSeq = parseSeqOver thomasBolducAndZachFOLFormulaParser
+    , ndParseForm = thomasBolducAndZachFOLFormulaParser
+    , ndNotation = ndNotation P.thomasBolducAndZachTFLCalc
+    }
+
+thomasBolducAndZachFOL2019Calc = mkNDCalc
+    { ndRenderer = FitchStyle StandardFitch
+    , ndParseProof = parseThomasBolducAndZachFOLCoreProof
     , ndProcessLine = hoProcessLineFitch
     , ndProcessLineMemo = Just hoProcessLineFitchMemo
     , ndParseSeq = parseSeqOver thomasBolducAndZachFOLFormulaParser
