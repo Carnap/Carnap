76aa8ad14ed94c85c63bc4751b61f6eb9118c036
gleachkr <gleachkr@gmail.com>
2019-11-05 14:29:22 -0600

    Allow >1 solution in translations. Closes #35.

--- Carnap-GHCJS/src/Carnap/GHCJS/Action/Translate.hs
+++ Carnap-GHCJS/src/Carnap/GHCJS/Action/Translate.hs
@@ -14,10 +14,10 @@ import Carnap.GHCJS.SharedTypes
 import Carnap.GHCJS.SharedFunctions
 import Data.IORef
 import qualified Data.Map as M
-import Data.Text (pack)
+import Data.Text (pack,Text)
 import Text.Parsec 
 import GHCJS.DOM
-import GHCJS.DOM.Types
+import GHCJS.DOM.Types hiding (Text)
 import GHCJS.DOM.Element
 import GHCJS.DOM.HTMLInputElement (HTMLInputElement, setValue, getValue,castToHTMLInputElement)
 import GHCJS.DOM.Document (Document,createElement, getBody, getDefaultView)
@@ -39,19 +39,19 @@ activateTranslate w (Just (i,o,opts)) = do
         case (M.lookup "transtype" opts, M.lookup "system" opts) of
             (Just "prop", mparser) -> activateWith formParser (tryTrans formParser) propChecker
                 where formParser = case mparser >>= ofPropSys ndParseForm of
-                                       Nothing -> purePropFormulaParser standardLetters <* eof
-                                       Just theParser -> theParser <* eof
+                                       Nothing -> purePropFormulaParser standardLetters
+                                       Just theParser -> theParser
             (Just "first-order", mparser) -> activateWith formParser (tryTrans formParser) folChecker
                 where formParser = case mparser >>= ofFOLSys ndParseForm of
-                                       Nothing -> folFormulaParser <* eof
-                                       Just theParser -> theParser <* eof
+                                       Nothing -> folFormulaParser
+                                       Just theParser -> theParser 
             _ -> return ()
     where optlist = case M.lookup "options" opts of Just s -> words s; Nothing -> []
           activateWith parser translator checker =
               case (M.lookup "goal" opts, M.lookup "content" opts, M.lookup "problem" opts) of
                   (Just g, Just content, Just problem) ->
-                    case parse parser "" (simpleDecipher . read $ g) of
-                      (Right f) -> do 
+                    case parse (parser `sepBy` (spaces >> char ',' >> spaces) <* eof) "" (simpleDecipher . read $ g) of
+                      (Right fs) -> do 
                            bw <- buttonWrapper w
                            ref <- newIORef False
                            case M.lookup "submission" opts of
@@ -59,14 +59,14 @@ activateTranslate w (Just (i,o,opts)) = do
                                   let l = Prelude.drop 7 s
                                   bt <- doneButton w "Submit Solution"
                                   appendChild bw (Just bt)
-                                  submit <- newListener $ submitTrans opts i ref l f parser checker
+                                  submit <- newListener $ submitTrans opts i ref l fs parser checker
                                   addListener bt click submit False                
                               _ -> return ()
                            setValue (castToHTMLInputElement i) (Just content)
                            setInnerHTML o (Just problem)
                            mpar@(Just par) <- getParentNode o               
                            insertBefore par (Just bw) (Just o)
-                           translate <- newListener $ translator checker o ref f
+                           translate <- newListener $ translator checker o ref fs
                            if "nocheck" `elem` optlist 
                                then return ()
                                else addListener i keyUp translate False                  
@@ -75,34 +75,36 @@ activateTranslate w (Just (i,o,opts)) = do
 activateChecker _ Nothing  = return ()
 
 tryTrans :: Eq (FixLang lex sem) => Parsec String () (FixLang lex sem) -> EquivTest lex sem 
-    -> Element -> IORef Bool -> FixLang lex sem -> EventM HTMLInputElement KeyboardEvent ()
-tryTrans parser equiv o ref f = onEnter $ 
+    -> Element -> IORef Bool -> [FixLang lex sem] -> EventM HTMLInputElement KeyboardEvent ()
+tryTrans parser equiv o ref fs = onEnter $ 
                 do (Just t) <- target :: EventM HTMLInputElement KeyboardEvent (Maybe HTMLInputElement)
                    (Just ival)  <- getValue t
                    case parse (spaces *> parser) "" ival of
                          Right f' -> liftIO $ checkForm f'
                          Left e -> message "Sorry, try again---that formula isn't gramatical."
    where checkForm f' 
-            | f' == f = do message "perfect match!"
-                           writeIORef ref True
-                           setInnerHTML o (Just "Success!")
-            | f' `equiv` f = do message "Logically equivalent to the standard translation"
+            | f' `elem` fs = do message "perfect match!"
                                 writeIORef ref True
                                 setInnerHTML o (Just "Success!")
+            | any (\f -> f' `equiv` f) fs = do message "Logically equivalent to a standard translation"
+                                               writeIORef ref True
+                                               setInnerHTML o (Just "Success!")
             | otherwise = message "Not quite. Try again!"
 
-submitTrans opts i ref l f parser checker = 
+submitTrans opts i ref l fs parser checker = 
         do isFinished <- liftIO $ readIORef ref
            if isFinished
-               then trySubmit Translation opts l (ProblemContent (pack $ show f)) True
+               then trySubmit Translation opts l (ProblemContent (pack $ show fs)) True
                else if ("exam" `elem` optlist) || ("nocheck" `elem` optlist)
                         then do (Just v) <- getValue (castToHTMLInputElement i)
                                 case parse parser "" v of
-                                    Right f' | checker f f' -> trySubmit Translation opts l (ProblemContent (pack $ show f)) True
-                                    _ | "exam" `elem` optlist -> trySubmit Translation opts l (TranslationDataOpts (pack $ show f) (pack v) (M.toList opts)) False
+                                    Right f' | any (\f -> checker f f') fs -> trySubmit Translation opts l (ProblemContent (serialize fs)) True
+                                    _ | "exam" `elem` optlist -> trySubmit Translation opts l (TranslationDataOpts (serialize fs) (pack v) (M.toList opts)) False
                                     _ -> message "something is wrong... try again?"
                         else message "not yet finished (remember to press return to check your work before submitting!)"
     where optlist = case M.lookup "options" opts of Just s -> words s; Nothing -> []
+          serialize :: Show a => [a] -> Text
+          serialize = pack . tail . init . show --we drop the list brackets
 
 propChecker f g = f == g || f `isEquivTo` g
 
