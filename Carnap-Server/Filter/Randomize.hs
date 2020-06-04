87741dc81115ad99461396c27162137d4f91b380
Graham Leach-Krouse <gleachkr@gmail.com>
2020-06-04 14:08:24 -0500

    Fix issue randomizing problems with point values.

--- Carnap-Server/Filter/Randomize.hs
+++ Carnap-Server/Filter/Randomize.hs
@@ -14,6 +14,6 @@ randomize :: Int -> [Block] -> [Block]
 randomize salt = map pickOne . groupBy sameEx
     where sameEx a b | Just x <- exercise a, Just y <- exercise b, x == y = True
                      | otherwise = False
-          exercise (Div (_,["exercise"],[]) (Plain [Span _ [Str h]]:_)) = Just h
+          exercise (Div (_,["exercise"],[]) (Plain (Span _ [Str h]:_):_)) = Just h
           exercise _ = Nothing
           pickOne l =  l !! (fst $ randomR (0,length l - 1) (mkStdGen (hashWithSalt salt (exercise $ head l))))
