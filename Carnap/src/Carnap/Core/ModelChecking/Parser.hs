75c6fb5bed964193f0def46d5ff03eec183a23b0
lf- <lf-@users.noreply.github.com>
2020-08-07 19:26:38 -0700

    Delete dead code

--- Carnap/src/Carnap/Core/ModelChecking/Parser.hs
+++
@@ -1,8 +0,0 @@
-module Carnap.Core.ModelChecking.Parser() where
-
-import Carnap.Core.ModelChecking.ModelFinder
-import Carnap.Languages.Util.LanguageClasses (BooleanLanguage)
-import Carnap.Languages.Util.GenericParsers
-import Text.Parsec
-import Text.Parsec.Expr
-import Text.Parsec.Char
