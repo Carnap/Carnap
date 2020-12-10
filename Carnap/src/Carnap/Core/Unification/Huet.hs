94f44d432d8a61daa7a575d384d13be65b8ffdaf
Graham Leach-Krouse <gleachkr@gmail.com>
2020-12-10 14:12:11 -0600

    Remove spurious over-specialization

--- Carnap/src/Carnap/Core/Unification/Huet.hs
+++ Carnap/src/Carnap/Core/Unification/Huet.hs
@@ -7,17 +7,15 @@ import Carnap.Core.Util
 import Carnap.Core.Data.Classes
 import Carnap.Core.Unification.Unification 
 import Data.Typeable
-import Data.List
+import Data.List (nub)
 import Control.Monad
-import Control.Monad.Identity
 import Control.Monad.Logic
-import Control.Monad.State
 import Control.Monad.Trans.Class as M
 
 -- | simplifies a set of equations, returning a set of equations with the same
 --set of solutions, but no rigid-rigid equations---or Nothing, in the case
 --where the set of equations has no solutions.
-simplify ::(HigherOrder f, MonadVar f (State Int)) => [Equation f] -> LogicT (State Int) [Equation f]
+simplify ::(HigherOrder f, MonadVar f m) => [Equation f] -> LogicT m [Equation f]
 simplify eqs = 
         do (rigid,flex) <- partitionM (\eq -> compareHeads eq []) eqs
            if null rigid 
@@ -40,7 +38,7 @@ massDecompose l = M.lift $ concat <$> mapM (\(x:=:y) -> statefulDecompose x y) l
 --(since these are guaranteed to have heads that are either constants or
 --variables), but also causes the computational branch to fail of there's
 --a mismatch of (constant) heads
-compareHeads :: (HigherOrder f, MonadVar f (State Int)) => Equation f -> [AnyPig f] -> LogicT (State Int) Bool
+compareHeads :: (HigherOrder f, MonadVar f m) => Equation f -> [AnyPig f] -> LogicT m Bool
 compareHeads (x:=:y) bv = case (castLam x, castLam y) of
                          (Just (ExtLam (l :: f t1 -> f t1') Refl),Just (ExtLam (l' :: f t2 -> f t2') Refl)) -> do 
                               case (eqT :: Maybe (t1 :~: t2)) of
@@ -70,7 +68,7 @@ compareHeads (x:=:y) bv = case (castLam x, castLam y) of
 --
 --bad behavior can be expected when given a rigid/rigid, flexible/flexible,
 --or rigid/flexible equation.
-generate :: (MonadVar f (State Int), HigherOrder f) => Equation f -> LogicT (State Int) (Equation f)
+generate :: (MonadVar f m, HigherOrder f) => Equation f -> LogicT m (Equation f)
 generate ((x :: f a) :=: y) = --accumulator for projection terms
          do case (castLam x, castLam y) of
                 (Just (ExtLam l Refl),Just (ExtLam l' Refl)) -> 
@@ -90,18 +88,16 @@ generate ((x :: f a) :=: y) = --accumulator for projection terms
                            Just Refl -> return (headX:=:newTerm)
                            Nothing -> mzero
 
-guillotine :: (HigherOrder f, Monad m,Typeable a, MonadVar f (State Int), EtaExpand f a) => f a -> m (AnyPig f, [AnyPig f])
+guillotine :: (HigherOrder f, Typeable a, Monad m, EtaExpand f a) => f a -> m (AnyPig f, [AnyPig f])
 guillotine x = basket (AnyPig x) []
             where basket (AnyPig x) pigs = case matchApp x of
                       Just (ExtApp h t) -> basket (AnyPig h) ((AnyPig t):pigs)
                       Nothing -> return (AnyPig x,pigs)
 
---Decompose using fresh variables, avoiding the binding-freshening step later
---
 --XXX: this inserts some vacuous lambdas. This could be avoided by using
 --`occurs sv` as a guard in reabstract, but it seems more efficient to just
 --leave them in (though there's no perceptible difference either way)
-statefulDecompose :: (HigherOrder f, Monad m,Typeable a, MonadVar f m, EtaExpand f a) => f a -> f a -> m [Equation f]
+statefulDecompose :: (HigherOrder f, Typeable a, MonadVar f m, EtaExpand f a) => f a -> f a -> m [Equation f]
 statefulDecompose a b = case (castLam a, castLam b) of
             (Just (ExtLam (f ::  f a -> f b) Refl), Just (ExtLam (f' :: f a' -> f b') Refl)) -> 
                 case (eqT :: Maybe (a:~:a'), eqT :: Maybe (b:~:b')) of
@@ -125,7 +121,7 @@ statefulDecompose a b = case (castLam a, castLam b) of
 --
 --Note that the projection term will not be of the same type as the return
 --value. Hence, we need an AnyPig here.
-project :: (MonadVar f (State Int), HigherOrder f) => [AnyPig f] -> (AnyPig f,AnyPig f) -> LogicT (State Int) (AnyPig f)
+project :: (MonadVar f m, HigherOrder f) => [AnyPig f] -> (AnyPig f,AnyPig f) -> LogicT m (AnyPig f)
 project pvs (AnyPig (var :: f vt), AnyPig (term :: f tt)) = 
         case eqT :: Maybe (vt :~: tt) of
             Just Refl -> do pigbodies <- M.lift $ handleBodyAbs pvs var term []
@@ -133,7 +129,7 @@ project pvs (AnyPig (var :: f vt), AnyPig (term :: f tt)) =
                             foldr mplus mzero (map return projections)
             Nothing -> error "var/term mismatch"
 
-imitate :: (MonadVar f (State Int), HigherOrder f) => [AnyPig f] -> AnyPig f -> LogicT (State Int) (AnyPig f)
+imitate :: (MonadVar f m, HigherOrder f) => [AnyPig f] -> AnyPig f -> LogicT m (AnyPig f)
 imitate pvs (AnyPig term) 
     | isVar term = mzero
     | otherwise = 
@@ -141,15 +137,15 @@ imitate pvs (AnyPig term)
            imitation <- M.lift $ bindAll (reverse pvs) (AnyPig body)
            return imitation 
 
-handleBodyApp :: (MonadVar f (State Int), HigherOrder f, Typeable a) => [AnyPig f] -> f a -> State Int (f a)
+handleBodyApp :: (MonadVar f m, HigherOrder f, Typeable a) => [AnyPig f] -> f a -> m (f a)
 handleBodyApp projvars term = case matchApp term of
    Nothing -> return term
    (Just (ExtApp h t)) -> do newInit <- handleBodyApp projvars h
                              freshArg <- genFreshArg projvars newInit
                              return $ newInit .$. freshArg
 
-handleBodyAbs :: (MonadVar f (State Int), HigherOrder f, EtaExpand f a, Typeable a) 
-    => [AnyPig f] -> f a -> f a -> [AnyPig f] ->  State Int [AnyPig f]
+handleBodyAbs :: (MonadVar f m, HigherOrder f, EtaExpand f a, Typeable a) 
+    => [AnyPig f] -> f a -> f a -> [AnyPig f] ->  m [AnyPig f]
 handleBodyAbs projvars var term acc = case castLam term of
    Nothing -> return (AnyPig var:acc)
    (Just (ExtLam f Refl)) -> do freshArg <- genFreshArg projvars term
@@ -161,7 +157,7 @@ toVars (AnyPig (x :: f t):xs) = do y :: f t <- fresh
                                    return (AnyPig y:tail)
 toVars [] = return []
 
-bindAll :: (HigherOrder f, MonadVar f (State Int)) => [AnyPig f] -> AnyPig f -> State Int (AnyPig f)
+bindAll :: (HigherOrder f, MonadVar f m) => [AnyPig f] -> AnyPig f -> m (AnyPig f)
 bindAll (AnyPig v :vs) (AnyPig body ) = 
             do f <- safesubst v body
                bindAll vs (AnyPig (lam f))
@@ -171,12 +167,12 @@ bindAll [] body = return body
 safesubst :: (HigherOrder f, MonadVar f m, Typeable a, Typeable b) => f a -> f b -> m (f a -> f b)
 safesubst (x :: f a) (y :: f b) = return $ \z -> subst x z y
 
-genFreshArg :: (MonadVar f (State Int), EtaExpand f a, HigherOrder f, Typeable a) => 
-    [AnyPig f] -> f (a -> b) -> State Int (f a)
+genFreshArg :: (MonadVar f m, EtaExpand f a, HigherOrder f, Typeable a) => 
+    [AnyPig f] -> f (a -> b) -> m (f a)
 genFreshArg projvars term =
         do (EveryPig head) <- freshPig 
            return $ attach head projvars
-    where attach ::  (HigherOrder f, Typeable d, MonadVar f (State Int), EtaExpand f d) 
+    where attach ::  (HigherOrder f, Typeable d, EtaExpand f d) 
             => (forall c . (Typeable c, EtaExpand f c) => f c) -> [AnyPig f] -> f d
           attach h  ((AnyPig v):vs) = attach (h .$. v) vs
           attach h [] = h
@@ -184,11 +180,11 @@ genFreshArg projvars term =
 --- | given x, y with no leading variables, this applies the generate rule
 --to replace the old head of x with the new head
 
-huetunify :: (HigherOrder f, MonadVar f (State Int))
+huetunify :: (HigherOrder f, MonadVar f m)
         => (forall a. f a -> Bool) --treat certain variables as constants
         -> [Equation f] --equations to be solved
         -> [Equation f] --accumulator for the substitution
-        -> LogicT (State Int) [Equation f]
+        -> LogicT m [Equation f]
 huetunify varConst [] ss = return (reverse ss)
 huetunify varConst es ss = 
         do seqs <- simplify es
@@ -201,7 +197,7 @@ huetunify varConst es ss =
                              huetunify varConst (filter (not . trivial) fresheqs) (genSub:ss)
     where trivial (x:=:y) = x =* y
 
-eqLMatch :: (MonadVar f (State Int), HigherOrder f) => Equation f -> (State Int) (Equation f)
+eqLMatch :: (MonadVar f m, HigherOrder f) => Equation f -> m (Equation f)
 eqLMatch (x :=: y) = 
         case (castLam x, castLam y) of
              (Nothing, Nothing) -> 
@@ -223,8 +219,8 @@ eqLMatch (x :=: y) =
                         (lam $ \z -> subst v z x') :=:
                         (lam $ \z -> subst v z y') 
                    
-eqFreshen :: (HigherOrder f, MonadVar f (State Int)) => Equation f -> (State Int) (Equation f)
+eqFreshen :: (HigherOrder f, MonadVar f m) => Equation f -> m (Equation f)
 eqFreshen ((x :: f a):=:y) =  do return (betaNormalizeByName x :=: y)
 
-huetUnifySys :: (MonadVar f (State Int), HigherOrder f) => (forall a. f a -> Bool) -> [Equation f] -> (State Int) [[Equation f]]
+huetUnifySys :: (MonadVar f m, HigherOrder f) => (forall a. f a -> Bool) -> [Equation f] -> m [[Equation f]]
 huetUnifySys varConst eqs = observeAllT (huetunify varConst eqs [])
