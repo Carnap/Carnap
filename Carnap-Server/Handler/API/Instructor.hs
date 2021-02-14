4c96340bfffc0cfe0e505e1b13c1a8501707618b
Graham Leach-Krouse <gleachkr@gmail.com>
2021-02-14 11:02:01 -0600

    Add PATCH to update document metadata via API

--- Carnap-Server/Handler/API/Instructor.hs
+++ Carnap-Server/Handler/API/Instructor.hs
@@ -36,25 +36,44 @@ postAPIInstructorDocumentsR ident = do Entity uid _ <- userFromIdent ident
 getAPIInstructorDocumentR :: Text -> DocumentId -> Handler Value
 getAPIInstructorDocumentR ident docid = do Entity uid _ <- userFromIdent ident 
                                            doc <- runDB (get docid) >>= maybe (sendStatusJSON notFound404 ("No such document" :: Text)) pure
-                                           if documentCreator doc == uid
-                                               then returnJson doc
-                                               else sendStatusJSON forbidden403 ("Document not owned by this instructor" :: Text)
+                                           checkUID doc uid
+                                           returnJson doc
+
+data DocumentPatch = DocumentPatch 
+                        { patchScope :: Maybe SharingScope
+                        , patchDescription :: Maybe (Maybe Text)
+                        }
+
+instance FromJSON DocumentPatch where
+        parseJSON = withObject "documentPatch" $ \o -> do
+            DocumentPatch <$> (o .:? "scope") 
+                          <*> (o .:! "description")
+
+patchAPIInstructorDocumentR :: Text -> DocumentId -> Handler Value
+patchAPIInstructorDocumentR ident docid = do Entity uid _ <- userFromIdent ident 
+                                             doc <- runDB (get docid) >>= maybe (sendStatusJSON notFound404 ("No such document" :: Text)) pure
+                                             checkUID doc uid
+                                             patch <- requireJsonBody :: Handler DocumentPatch
+                                             doc' <- runDB $ updateGet docid 
+                                                           $ maybeUpdate DocumentScope (patchScope patch)
+                                                          ++ maybeUpdate DocumentDescription (patchDescription patch)
+                                             returnJson doc'
+    where maybeUpdate field (Just val) = [field =. val]
+          maybeUpdate field Nothing = []
 
 getAPIInstructorDocumentDataR :: Text -> DocumentId -> Handler Value
 getAPIInstructorDocumentDataR ident docid = do Entity uid _ <- userFromIdent ident 
                                                doc <- runDB (get docid) >>= maybe (sendStatusJSON notFound404 ("No such document" :: Text)) pure
-                                               if documentCreator doc == uid
-                                                   then docFilePath ident doc >>= asFile doc 
-                                                   else sendStatusJSON forbidden403 ("Document not owned by this instructor" :: Text)
+                                               checkUID doc uid
+                                               docFilePath ident doc >>= asFile doc 
 
 putAPIInstructorDocumentDataR :: Text -> DocumentId -> Handler Value
 putAPIInstructorDocumentDataR ident docid = do Entity uid _ <- userFromIdent ident 
                                                doc <- runDB (get docid) >>= maybe (sendStatusJSON notFound404 ("No such document" :: Text)) pure
-                                               if documentCreator doc == uid
-                                                   then do path <- docFilePath ident doc 
-                                                           connect rawRequestBody (sinkFile path)
-                                                           sendStatusJSON created201 ("contents updated" :: Text)
-                                                   else sendStatusJSON forbidden403 ("Document not owned by this instructor" :: Text)
+                                               checkUID doc uid
+                                               path <- docFilePath ident doc 
+                                               connect rawRequestBody (sinkFile path)
+                                               sendStatusJSON created201 ("contents updated" :: Text)
 
 userFromIdent :: Text -> Handler (Entity User)
 userFromIdent ident = runDB (getBy $ UniqueUser ident) >>= maybe (sendStatusJSON notFound404 ("No such instructor" :: Text)) pure
@@ -62,3 +81,6 @@ userFromIdent ident = runDB (getBy $ UniqueUser ident) >>= maybe (sendStatusJSON
 docFilePath :: Text -> Document -> Handler FilePath
 docFilePath ident doc = do datadir <- appDataRoot <$> (appSettings <$> getYesod)
                            return (datadir </> "documents" </> unpack ident </> unpack (documentFilename doc))
+
+checkUID doc uid | documentCreator doc == uid = return ()
+                 | otherwise = sendStatusJSON forbidden403 ("Document not owned by this instructor" :: Text)
