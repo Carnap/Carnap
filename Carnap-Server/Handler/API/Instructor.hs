9899c53d349fb441ff4bdabaae6bfab484c73e77
Graham Leach-Krouse <gleachkr@gmail.com>
2021-02-19 11:52:39 -0600

    more uniform reply messages

--- Carnap-Server/Handler/API/Instructor.hs
+++ Carnap-Server/Handler/API/Instructor.hs
@@ -35,7 +35,7 @@ postAPIInstructorDocumentsR ident = do Entity uid _ <- userFromIdent ident
                                                liftIO (doesFileExist path) >>= --don't clobber and annex
                                                    bool (return ()) (sendStatusJSON conflict409 ("A document with that name already exists." :: Text)) 
                                                inserted <- runDB (insertUnique doc) >>=
-                                                           maybe (sendStatusJSON conflict409 ("A document with that name already exists" :: Text)) return
+                                                           maybe (sendStatusJSON conflict409 ("A document with that name already exists." :: Text)) return
                                                writeFile path " " 
                                                render <- getUrlRender
                                                addHeader "Location" (render $ APIInstructorDocumentR ident inserted)
