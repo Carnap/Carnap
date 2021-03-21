Carnap/Carnap

commit 96c25264a4307597b50ab17ca5792b1e3110db84

Reorganize some runtimeConfig code

Carnap/src/Carnap/Calculi/Util.hs
@@ -2,6 +2,7 @@
 module Carnap.Calculi.Util where
 
 import Data.Tree
+import Data.Map (Map)
 import Carnap.Core.Data.Types
 import Carnap.Core.Data.Classes
 import Carnap.Core.Unification.Unification
@@ -44,6 +45,13 @@ data ProofErrorMessage :: ((* -> *) -> * -> *) -> * where
         NoUnify :: [[Equation (ClassicalSequentOver lex)]]  -> Int -> ProofErrorMessage lex
         GenericError :: String -> Int -> ProofErrorMessage lex
         NoResult :: Int -> ProofErrorMessage lex --meant for blanks
+
+data RuntimeDeductionConfig lex sem = RuntimeDeductionConfig
+        { derivedRules :: Map String (ClassicalSequentOver lex (Sequent sem))
+        , problemPremises :: Maybe [ClassicalSequentOver lex (Sequent sem)]
+        }
+
+defaultRuntimeDeductionConfig = defaultRuntimeDeductionConfig
 
 data TreeFeedbackNode lex = Correct | Waiting | ProofData String | ProofError (ProofErrorMessage lex)
 

Carnap-GHCJS/src/Carnap/GHCJS/Action/AcceptJSON.hs
@@ -23,6 +23,7 @@ import GHCJS.DOM
 import GHCJS.DOM.Types
 import Carnap.Core.Data.Types (Form, FixLang,)
 import Carnap.Core.Data.Classes (Schematizable,UniformlyEq(..))
+import Carnap.Calculi.Util
 import Carnap.Calculi.NaturalDeduction.Syntax
 import Carnap.Calculi.NaturalDeduction.Parser
 import Carnap.Calculi.NaturalDeduction.Checker
@@ -110,14 +111,14 @@ parseProofData parsePair valList = evalStateT (process valList) 1
                      return (wff,jst')
 
 parsePairProp (wff,jstr) = AssertLine <$> P.parse (purePropFormulaParser extendedLetters) "" wff
-                                      <*> (fst <$> P.parse (parseJstr $ parseMagnusSL (RuntimeNaturalDeductionConfig mempty mempty)) "" jstr)
+                                      <*> (fst <$> P.parse (parseJstr $ parseMagnusSL (defaultRuntimeDeductionConfig)) "" jstr)
                                       <*> return 0
-                                      <*> (snd <$> P.parse (parseJstr $ parseMagnusSL (RuntimeNaturalDeductionConfig mempty mempty)) "" jstr)
+                                      <*> (snd <$> P.parse (parseJstr $ parseMagnusSL (defaultRuntimeDeductionConfig)) "" jstr)
 
 parsePairFOL  (wff,jstr) = AssertLine <$> P.parse magnusFOLFormulaParser "" wff
-                                      <*> (fst <$> P.parse (parseJstr $ parseMagnusQL (RuntimeNaturalDeductionConfig mempty mempty)) "" jstr)
+                                      <*> (fst <$> P.parse (parseJstr $ parseMagnusQL (defaultRuntimeDeductionConfig)) "" jstr)
                                       <*> return 0
-                                      <*> (snd <$> P.parse (parseJstr $ parseMagnusQL (RuntimeNaturalDeductionConfig mempty mempty)) "" jstr)
+                                      <*> (snd <$> P.parse (parseJstr $ parseMagnusQL (defaultRuntimeDeductionConfig)) "" jstr)
 
 parseJstr r = do rule <- spaces *> r
                  deps <- spaces *> many (try parseIntPair <|> parseInt)

Carnap-GHCJS/src/Carnap/GHCJS/Action/ProofCheck.hs
@@ -1,8 +1,9 @@
 {-#LANGUAGE FlexibleContexts, RankNTypes #-}
 module Carnap.GHCJS.Action.ProofCheck (proofCheckAction) where
 
+import Carnap.Calculi.Util (RuntimeDeductionConfig(..), defaultRuntimeDeductionConfig)
 import Carnap.Calculi.NaturalDeduction.Syntax 
-    (ProofMemoRef, NaturalDeductionCalc(..),RenderStyle(..), Inference(..), RuntimeNaturalDeductionConfig(..), Deduction)
+    (ProofMemoRef, NaturalDeductionCalc(..),RenderStyle(..), Inference(..), Deduction)
 import Carnap.Calculi.NaturalDeduction.Checker 
     (ProofErrorMessage(..), Feedback(..), seqSubsetUnify, toDisplaySequenceMemo, toDisplaySequence)
 import Carnap.Core.Data.Optics (liftLang, PrismSubstitutionalVariable, PrismLink)
@@ -85,7 +86,7 @@ data CheckerParameters r lex sem = CheckerParameters
         { checkerToRule :: Maybe (DerivedRule lex sem -> SomeRule)
         , checkerTests :: Maybe ([String] -> UnaryTest lex sem)
         , ruleSave' :: Maybe (CheckerParameters r lex sem -> IORef [(String, SomeRule)] -> IORef Bool -> Document -> Element -> EventM Element MouseEvent ())
-        , runtimeConfig' :: CheckerParameters r lex sem -> IO (RuntimeNaturalDeductionConfig lex sem)
+        , runtimeConfig' :: CheckerParameters r lex sem -> IO (RuntimeDeductionConfig lex sem)
         , checkerCalc ::  NaturalDeductionCalc r lex sem 
         , checkerRules :: IORef [(String,SomeRule)]
         , sequent :: Maybe (ClassicalSequentOver lex (Sequent sem))
@@ -161,7 +162,7 @@ activateChecker drs w (Just iog@(IOGoal i o g _ opts)) -- TODO: need to update n
                                                   let seqrules = catMaybes $ map readyRule somerules
                                                       rmap = M.fromList seqrules
                                                       premseqs = toPremiseSeqs . sequent $ self
-                                                  return $ RuntimeNaturalDeductionConfig rmap premseqs
+                                                  return $ RuntimeDeductionConfig rmap premseqs
                     where readyRule (x, PropRule r) = Just (x, derivedRuleToSequent r)
                           readyRule _ = Nothing
 
@@ -170,7 +171,7 @@ activateChecker drs w (Just iog@(IOGoal i o g _ opts)) -- TODO: need to update n
                                                  let seqrules = catMaybes $ map readyRule somerules
                                                      rmap = M.fromList seqrules
                                                      premseqs = toPremiseSeqs . sequent $ self
-                                                 return $ RuntimeNaturalDeductionConfig rmap premseqs
+                                                 return $ RuntimeDeductionConfig rmap premseqs
                     where readyRule (x, PropRule r) = Just (x, liftSequent . derivedRuleToSequent $ r)
                           readyRule (x, FOLRule r) = Just (x, derivedRuleToSequent r)
                           readyRule _ = Nothing
@@ -179,7 +180,7 @@ activateChecker drs w (Just iog@(IOGoal i o g _ opts)) -- TODO: need to update n
               toPremiseSeqs (Just seq) = Just . map (\x -> SA x :|-: SS x) $ toListOf (lhs . concretes) seq
               toPremiseSeqs Nothing = Nothing
 
-              noRuntimeOptions = CheckerParameters Nothing Nothing Nothing $ const . pure $ RuntimeNaturalDeductionConfig mempty mempty
+              noRuntimeOptions = CheckerParameters Nothing Nothing Nothing $ const . pure $ defaultRuntimeDeductionConfig
 
 threadedCheck checker options w ref v (g, fd) = 
         do mt <- readIORef (threadRef checker)
@@ -336,7 +337,7 @@ getFeedback checker ded = case ndProcessLineMemo calc of
                                      Nothing -> return $ toDisplaySequence (ndProcessLine calc) ded
     where calc = checkerCalc checker
 
-configFrom rules prems = RuntimeNaturalDeductionConfig (M.fromList . map (\(x,y) -> (x,derivedRuleToSequent y)) $ rules) prems
+configFrom rules prems = RuntimeDeductionConfig (M.fromList . map (\(x,y) -> (x,derivedRuleToSequent y)) $ rules) prems
 
 wrap calc fd w elt (Right s) = popUpWith fd w elt "+" (ndNotation calc $ show s) Nothing
 wrap _ fd w elt (Left (GenericError s n)) = popUpWith fd w elt "?" ("Error on line " ++ show n ++ ": " ++ s) Nothing

Carnap-GHCJS/src/Carnap/GHCJS/Action/SequentCheck.hs
@@ -214,7 +214,7 @@ toTableau calc (Node (l,r) f)
           newNode = case TableauNode <$> parsedLabel <*> (pure Nothing) <*> parsedRules of
                         Right n -> Right n
                         Left e -> Left (Node (ProofError $ NoParse e 0) (map cleanTree parsedForest))
-          rtc = RuntimeNaturalDeductionConfig mempty mempty --XXX: stub for future use
+          rtc = defaultRuntimeDeductionConfig --XXX: stub for future use
 
 renewRoot root calc = do
     mr <- toCleanVal root

Carnap-GHCJS/src/Carnap/GHCJS/Action/TreeDeductionCheck.hs
@@ -46,7 +46,7 @@ data CheckerParameters rule lex sem = CheckerParameters
             { tableauCalc :: TableauCalc lex sem rule
             , threadRef :: IORef (Maybe ThreadId)
             , proofMemo :: ProofMemoRef lex sem rule
-            , runtimeConfig' :: CheckerParameters rule lex sem -> IO (RuntimeNaturalDeductionConfig lex sem)
+            , runtimeConfig' :: CheckerParameters rule lex sem -> IO (RuntimeDeductionConfig lex sem)
             }
 
 runtimeConfig x = runtimeConfig' x x 
@@ -69,7 +69,7 @@ treeDeductionCheckAction =
                              { threadRef = ref
                              , proofMemo = memo
                              , tableauCalc = calc
-                             , runtimeConfig' = \_ -> return (RuntimeNaturalDeductionConfig mempty mempty)
+                             , runtimeConfig' = \_ -> return (defaultRuntimeDeductionConfig)
                              }))
 
 getCheckers :: IsElement self => Document -> self -> IO [Maybe (Element, Element, Map String String)]
@@ -104,7 +104,7 @@ activateChecker w (Just (i, o, opts)) = case (setupWith `ofPropTreeSys` sys)
                         { tableauCalc = calc
                         , threadRef = theThreadRef
                         , proofMemo = memo
-                        , runtimeConfig' = \_ -> return (RuntimeNaturalDeductionConfig mempty mempty)
+                        , runtimeConfig' = \_ -> return (defaultRuntimeDeductionConfig)
                         }
                   let submit = submitTree w checkerParams opts root mgoal
                   btStatus <- createSubmitButton w bw submit opts
@@ -194,7 +194,7 @@ toProofTree :: ( Typeable sem
                , Sequentable lex
                , StructuralOverride rule (ProofTree rule lex sem)
                , Inference rule lex sem
-               ) => RuntimeNaturalDeductionConfig lex sem -> CheckerParameters rule lex sem -> Tree (String,String) -> Either (TreeFeedback lex) (ProofTree rule lex sem)
+               ) => RuntimeDeductionConfig lex sem -> CheckerParameters rule lex sem -> Tree (String,String) -> Either (TreeFeedback lex) (ProofTree rule lex sem)
 toProofTree rtc checkerParams (Node (l,r) f) 
     | all isRight parsedForest && isRight newNode = handleOverride <$> (Node <$> newNode <*> sequence parsedForest)
     | isRight newNode = Left $ Node Waiting (map cleanTree parsedForest)

Carnap/src/Carnap/Calculi/NaturalDeduction/Syntax.hs
@@ -266,14 +266,9 @@ type ProofMemoRef lex sem r = IORef (Map Int [Either (ProofErrorMessage lex)
                                                      , r)]
                                            ])
 
-data RuntimeNaturalDeductionConfig lex sem = RuntimeNaturalDeductionConfig
-        { derivedRules :: Map String (ClassicalSequentOver lex (Sequent sem))
-        , problemPremises :: Maybe [ClassicalSequentOver lex (Sequent sem)]
-        }
-
 data NaturalDeductionCalc r lex sem = NaturalDeductionCalc 
         { ndRenderer :: RenderStyle
-        , ndParseProof :: RuntimeNaturalDeductionConfig lex sem
+        , ndParseProof :: RuntimeDeductionConfig lex sem
                             -> String -> [DeductionLine r lex sem]
         , ndProcessLine :: (Sequentable lex , Inference r lex sem, MonadVar (ClassicalSequentOver lex) (State Int))
                                 => Deduction r lex sem -> Restrictor r lex -> Int -> FeedbackLine lex sem

Carnap/src/Carnap/Calculi/Tableau/Data.hs
@@ -2,7 +2,7 @@ module Carnap.Calculi.Tableau.Data where
 
 import Carnap.Core.Data.Types
 import Carnap.Languages.ClassicalSequent.Syntax
-import Carnap.Calculi.NaturalDeduction.Syntax (RuntimeNaturalDeductionConfig)
+import Carnap.Calculi.Util (RuntimeDeductionConfig)
 import Data.Tree
 import Text.Parsec
 
@@ -35,7 +35,7 @@ type Tableau lex sem rule = Tree (TableauNode lex sem rule)
 
 data TableauCalc lex sem rule = TableauCalc 
            { tbParseForm :: Parsec String () (FixLang lex sem)
-           , tbParseRule :: RuntimeNaturalDeductionConfig lex sem -> Parsec String () [rule]
+           , tbParseRule :: RuntimeDeductionConfig lex sem -> Parsec String () [rule]
            , tbNotation :: String -> String
            }
 

Carnap/src/Carnap/Languages/DefiniteDescription/Logic/Gamut.hs
@@ -14,6 +14,7 @@ import Carnap.Languages.PureFirstOrder.Logic.Rules
 import Carnap.Languages.PurePropositional.Logic.Rules (fitchAssumptionCheck)
 import Carnap.Languages.ClassicalSequent.Syntax
 import Carnap.Languages.ClassicalSequent.Parser
+import Carnap.Calculi.Util
 import Carnap.Calculi.NaturalDeduction.Syntax
 import Carnap.Calculi.NaturalDeduction.Parser
 import Carnap.Calculi.NaturalDeduction.Checker (hoProcessLineFitchMemo, hoProcessLineFitch)
@@ -71,14 +72,14 @@ instance Inference GamutNDDesc FregeanDescLex (Form Bool) where
         isAssumption _ = False
 
 parseGamutNDDesc rtc = try ndRule <|> descRule
-    where ndRule = map GNDP <$> parseGamutNDPlus (RuntimeNaturalDeductionConfig mempty mempty)
+    where ndRule = map GNDP <$> parseGamutNDPlus (defaultRuntimeDeductionConfig)
           descRule = do r <- choice (map (try . string) ["Ii","Ei", "S="])
                         return $ case r of
                             "Ii" -> [InDD1, InDD2]
                             "Ei" -> [ElimDD1, ElimDD2]
                             "S=" -> [EqS]
 
-parseGamutNDDescProof :: RuntimeNaturalDeductionConfig FregeanDescLex (Form Bool) -> String -> [DeductionLine GamutNDDesc FregeanDescLex (Form Bool)]
+parseGamutNDDescProof :: RuntimeDeductionConfig FregeanDescLex (Form Bool) -> String -> [DeductionLine GamutNDDesc FregeanDescLex (Form Bool)]
 parseGamutNDDescProof rtc = toDeductionFitch (parseGamutNDDesc rtc) (gamutNDDescFormulaParser)
 
 gamutNDDescCalc = mkNDCalc

commit a876f56e11624e5ff159b32437a1329bc3dbde22

Fix dumb bug introduced by 96c25264a4307597b50ab17ca5792b1e3110db84

Carnap/src/Carnap/Calculi/Util.hs
@@ -51,7 +51,7 @@ data RuntimeDeductionConfig lex sem = RuntimeDeductionConfig
         , problemPremises :: Maybe [ClassicalSequentOver lex (Sequent sem)]
         }
 
-defaultRuntimeDeductionConfig = defaultRuntimeDeductionConfig
+defaultRuntimeDeductionConfig = RuntimeDeductionConfig mempty mempty
 
 data TreeFeedbackNode lex = Correct | Waiting | ProofData String | ProofError (ProofErrorMessage lex)
 