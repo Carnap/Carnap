57db2932d199ca34b974d00508319e4bd5202bfe
Graham Leach-Krouse <gleachkr@gmail.com>
2021-04-23 11:30:14 -0500

    Expose PaulPD, PaulPDE

--- Carnap/src/Carnap/Languages/PureFirstOrder/Logic.hs
+++ Carnap/src/Carnap/Languages/PureFirstOrder/Logic.hs
@@ -23,61 +23,64 @@ import Carnap.Calculi.Util
 import Carnap.Languages.PureFirstOrder.Syntax
 import Carnap.Calculi.NaturalDeduction.Syntax
 import Carnap.Calculi.Tableau.Data
-import Carnap.Languages.PureFirstOrder.Logic.Carnap
-import Carnap.Languages.PureFirstOrder.Logic.Magnus
-import Carnap.Languages.PureFirstOrder.Logic.KalishAndMontague
-import Carnap.Languages.PureFirstOrder.Logic.Gallow
-import Carnap.Languages.PureFirstOrder.Logic.ThomasBolducAndZach
-import Carnap.Languages.PureFirstOrder.Logic.EbelsDuggan
 import Carnap.Languages.PureFirstOrder.Logic.BergmannMoorAndNelson
-import Carnap.Languages.PureFirstOrder.Logic.Hausman
+import Carnap.Languages.PureFirstOrder.Logic.Bonevac
+import Carnap.Languages.PureFirstOrder.Logic.Carnap
+import Carnap.Languages.PureFirstOrder.Logic.EbelsDuggan
+import Carnap.Languages.PureFirstOrder.Logic.Equivalence
+import Carnap.Languages.PureFirstOrder.Logic.Gallow
 import Carnap.Languages.PureFirstOrder.Logic.Gamut
+import Carnap.Languages.PureFirstOrder.Logic.Gentzen
+import Carnap.Languages.PureFirstOrder.Logic.Goldfarb
+import Carnap.Languages.PureFirstOrder.Logic.Hardegree
+import Carnap.Languages.PureFirstOrder.Logic.Hausman
 import Carnap.Languages.PureFirstOrder.Logic.HowardSnyder
 import Carnap.Languages.PureFirstOrder.Logic.Hurley
-import Carnap.Languages.PureFirstOrder.Logic.Hardegree
-import Carnap.Languages.PureFirstOrder.Logic.Bonevac
-import Carnap.Languages.PureFirstOrder.Logic.Goldfarb
-import Carnap.Languages.PureFirstOrder.Logic.Tomassi
 import Carnap.Languages.PureFirstOrder.Logic.IchikawaJenkins
-import Carnap.Languages.PureFirstOrder.Logic.Gentzen
+import Carnap.Languages.PureFirstOrder.Logic.KalishAndMontague
+import Carnap.Languages.PureFirstOrder.Logic.Magnus
 import Carnap.Languages.PureFirstOrder.Logic.OpenLogic
-import Carnap.Languages.PureFirstOrder.Logic.Equivalence
+import Carnap.Languages.PureFirstOrder.Logic.Paul
+import Carnap.Languages.PureFirstOrder.Logic.ThomasBolducAndZach
+import Carnap.Languages.PureFirstOrder.Logic.Tomassi
 import Carnap.Languages.PureFirstOrder.Logic.Winkler 
 import Carnap.Languages.PureFirstOrder.Logic.Rules
 
 ofFOLSys :: (forall r . (Show r, Inference r PureLexiconFOL (Form Bool)) => 
              NaturalDeductionCalc r PureLexiconFOL (Form Bool) -> a) -> String -> Maybe a
-ofFOLSys f sys | sys == "firstOrder"                      = Just $ f folCalc
-               | sys == "montagueQC"                      = Just $ f montagueQCCalc
-               | sys == "magnusQL"                        = Just $ f magnusQLCalc
-               | sys == "magnusQLPlus"                    = Just $ f magnusQLPlusCalc
-               | sys == "thomasBolducAndZachFOL"          = Just $ f thomasBolducAndZachFOLCalc
-               | sys == "thomasBolducAndZachFOLCore"      = Just $ f thomasBolducAndZachFOLCoreCalc
-               | sys == "thomasBolducAndZachFOL2019"      = Just $ f thomasBolducAndZachFOL2019Calc
-               | sys == "thomasBolducAndZachFOLPlus2019"  = Just $ f thomasBolducAndZachFOLPlus2019Calc
-               | sys == "ebelsDugganFOL"                  = Just $ f ebelsDugganFOLCalc
-               | sys == "LogicBookPD"                     = Just $ f logicBookPDCalc
-               | sys == "LogicBookPDPlus"                 = Just $ f logicBookPDPlusCalc
+ofFOLSys f sys | sys == "LogicBookPD"                     = Just $ f logicBookPDCalc
                | sys == "LogicBookPDE"                    = Just $ f logicBookPDECalc
                | sys == "LogicBookPDEPlus"                = Just $ f logicBookPDEPlusCalc
-               | sys == "hausmanPL"                       = Just $ f hausmanPLCalc
-               | sys == "gamutND"                         = Just $ f gamutNDCalc
-               | sys == "gamutNDPlus"                     = Just $ f gamutNDPlusCalc
+               | sys == "LogicBookPDPlus"                 = Just $ f logicBookPDPlusCalc
+               | sys == "bonevacQL"                       = Just $ f bonevacQLCalc
+               | sys == "ebelsDugganFOL"                  = Just $ f ebelsDugganFOLCalc
+               | sys == "firstOrder"                      = Just $ f folCalc
                | sys == "gallowPL"                        = Just $ f gallowPLCalc
                | sys == "gallowPLPlus"                    = Just $ f gallowPLPlusCalc
+               | sys == "gamutND"                         = Just $ f gamutNDCalc
+               | sys == "gamutNDPlus"                     = Just $ f gamutNDPlusCalc
+               | sys == "goldfarbAltND"                   = Just $ f goldfarbAltNDCalc
+               | sys == "goldfarbAltNDPlus"               = Just $ f goldfarbAltNDPlusCalc
+               | sys == "goldfarbND"                      = Just $ f goldfarbNDCalc
+               | sys == "goldfarbNDPlus"                  = Just $ f goldfarbNDPlusCalc
+               | sys == "hardegreePL"                     = Just $ f hardegreePLCalc
+               | sys == "hardegreePL2006"                 = Just $ f hardegreePL2006Calc
+               | sys == "hausmanPL"                       = Just $ f hausmanPLCalc
                | sys == "howardSnyderPL"                  = Just $ f howardSnyderPLCalc
                | sys == "hurleyPL"                        = Just $ f hurleyPLCalc
                | sys == "ichikawaJenkinsQL"               = Just $ f ichikawaJenkinsQLCalc
-               | sys == "hardegreePL"                     = Just $ f hardegreePLCalc
-               | sys == "hardegreePL2006"                 = Just $ f hardegreePL2006Calc
-               | sys == "bonevacQL"                       = Just $ f bonevacQLCalc
-               | sys == "goldfarbND"                      = Just $ f goldfarbNDCalc
-               | sys == "goldfarbAltND"                   = Just $ f goldfarbAltNDCalc
-               | sys == "goldfarbNDPlus"                  = Just $ f goldfarbNDPlusCalc
-               | sys == "goldfarbAltNDPlus"               = Just $ f goldfarbAltNDPlusCalc
+               | sys == "magnusQL"                        = Just $ f magnusQLCalc
+               | sys == "magnusQLPlus"                    = Just $ f magnusQLPlusCalc
+               | sys == "montagueQC"                      = Just $ f montagueQCCalc
+               | sys == "paulPD"                          = Just $ f paulPDCalc
+               | sys == "paulPDE"                         = Just $ f paulPDECalc
+               | sys == "thomasBolducAndZachFOL"          = Just $ f thomasBolducAndZachFOLCalc
+               | sys == "thomasBolducAndZachFOL2019"      = Just $ f thomasBolducAndZachFOL2019Calc
+               | sys == "thomasBolducAndZachFOLCore"      = Just $ f thomasBolducAndZachFOLCoreCalc
+               | sys == "thomasBolducAndZachFOLPlus2019"  = Just $ f thomasBolducAndZachFOLPlus2019Calc
                | sys == "tomassiQL"                       = Just $ f tomassiQLCalc
-               | sys == "zachFOLEq"                       = Just $ f zachFOLEqCalc
                | sys == "winklerFOL"                      = Just $ f winklerFOLCalc
+               | sys == "zachFOLEq"                       = Just $ f zachFOLEqCalc
                | otherwise                                = Nothing
 
 ofFOLTreeSys :: (forall r . 
