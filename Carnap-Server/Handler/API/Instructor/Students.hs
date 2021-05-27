d8a516bb83894339a63d41a8d0e8966f43e9609a
Graham Leach-Krouse <gleachkr@gmail.com>
2021-05-27 11:13:11 -0500

    Add assignment access token deletion

--- Carnap-Server/Handler/API/Instructor/Students.hs
+++ Carnap-Server/Handler/API/Instructor/Students.hs
@@ -47,8 +47,19 @@ getAPIInstructorStudentAssignmentTokensR :: Text -> Text -> UserDataId -> Handle
 getAPIInstructorStudentAssignmentTokensR ident coursetitle udid = do 
              courseEnt@(Entity cid course) <- canAccessClass ident coursetitle
              tokens <- runDB $ do ud <- studentEnrolled udid cid
-                                  selectList [AssignmentAccessTokenUser ==. userDataUserId ud] []
+                                  alltokens <- selectList [AssignmentAccessTokenUser ==. userDataUserId ud] []
+                                  filterM (\(Entity _ tok) -> tokenCourse tok >>= \mcid -> return (mcid == Just cid)) alltokens
              returnJson tokens
+
+deleteAPIInstructorStudentAssignmentTokenR :: Text -> Text -> UserDataId -> AssignmentAccessTokenId -> Handler Value
+deleteAPIInstructorStudentAssignmentTokenR ident coursetitle udid tokid = do 
+             courseEnt@(Entity cid course) <- canAccessClass ident coursetitle
+             runDB $ do tok <- get tokid >>= maybe (sendStatusJSON notFound404 ("No available token for this id" :: Text)) pure
+                        mcid <- tokenCourse tok
+                        if mcid == Just cid 
+                            then delete tokid
+                            else sendStatusJSON notFound404 ("No available token for this id" :: Text)
+             returnJson ("deleted token" :: Text)
 
 
 studentEnrolled :: (YesodPersist site, YesodPersistBackend site ~ SqlBackend) => UserDataId -> CourseId -> YesodDB site UserData
@@ -56,3 +67,6 @@ studentEnrolled udid cid = do
         ud <- get udid >>= maybe (sendStatusJSON notFound404 ("No userdata for this ident" :: Text)) pure
         if userDataEnrolledIn ud == Just cid then return () else sendStatusJSON notFound404 ("No userdata for this ident" :: Text)
         return ud
+
+tokenCourse :: (YesodPersist site, YesodPersistBackend site ~ SqlBackend) => AssignmentAccessToken -> YesodDB site (Maybe CourseId)
+tokenCourse tok = get (assignmentAccessTokenAssignment tok) >>= return . maybe Nothing (Just . assignmentMetadataCourse)
