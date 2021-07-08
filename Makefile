Carnap/Carnap

commit ac1b56f54e1e0f238cb6b8575a53b91899328e5c

Make cabal flags, *ROOT configurable

Makefile
@@ -1,6 +1,11 @@
 .PHONY: help devel run shell-ghc build-ghc shell-ghcjs build-ghcjs tags build-production hoogle
 TARGET := all
 
+CABALFLAGS := -f dev
+APPROOT := "http://localhost:3000"
+DATAROOT := "../dataroot"
+BOOKROOT := "../books/Carnap-Book/"
+
 help:
 	@echo "Usage: make [options] [target]"
 	@echo ""
@@ -47,9 +52,9 @@ else
 	cd Carnap-Server && \
 	cp -n config/settings-example.yml config/settings.yml && \
 	mkdir -p ../dataroot && \
-	APPROOT="http://localhost:3000" DATAROOT="../dataroot" \
-		BOOKROOT="../Carnap-Book/" \
-		cabal run -f dev Carnap-Server
+	APPROOT=$(APPROOT) DATAROOT=$(DATAROOT) \
+		BOOKROOT=$(BOOKROOT) \
+		cabal run $(CABALFLAGS) Carnap-Server $(CONFIGFILE)
 endif
 
 shell-ghc:

commit 0b39f4d27b3d29f38287e1291724fd4b6b1ca430

Fix incorrect BOOKROOT in Makefile
This regressed in ac1b56f where I accidentally committed a change from
the UBC fork where we reorganized the books. Sorry!

I'm not reproducing the book reorganization here because I too want to
kill the book route and don't want to give any excuses to keep it alive.

Makefile
@@ -47,7 +47,7 @@ devel: build-ghcjs run
 
 APPROOT := "http://localhost:3000"
 DATAROOT := "../dataroot"
-BOOKROOT := "../books/Carnap-Book/"
+BOOKROOT := "../Carnap-Book/"
 
 run:
 ifeq ($(origin NIX_STORE),undefined)