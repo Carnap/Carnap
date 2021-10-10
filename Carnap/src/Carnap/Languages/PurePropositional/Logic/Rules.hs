820314d2eb5330b19cd3bee51aa901eecee6de02
Graham Leach-Krouse <gleachkr@gmail.com>
2021-10-10 12:11:10 -0500

    Fix two rule issues
    1. an operator was backwards in dischargeConstraints
    2. explicitNonConstructiveReductioVariations was discharging the wrong
       form of premise

--- Carnap/src/Carnap/Languages/PurePropositional/Logic/Rules.hs
+++ Carnap/src/Carnap/Languages/PurePropositional/Logic/Rules.hs
@@ -44,7 +44,7 @@ premConstraint (Just prems) sub | theinstance `elem` prems = Nothing
     where theinstance = betaNormalizeByName $ applySub sub $ (SA (phin 1) :|-: SS (phin 1))
           project = view rhs
 
-dischargeConstraint n ded lhs sub | and (map (`elem` forms) lhs') = Nothing
+dischargeConstraint n ded lhs sub | all (`elem` lhs') forms = Nothing
                                   | otherwise = Just $ "Some of the stated dependencies in " ++ show forms ++ ", are not among the inferred dependencies " ++ show lhs' ++  "."
     where lhs' = toListOf concretes . applySub sub $ lhs
           scope = inScope (ded !! (n - 1))
@@ -409,7 +409,7 @@ explictConstructiveConjunctionReductioVariations = [
 explictNonConstructiveConjunctionReductioVariations :: BooleanRuleVariants lex b
 explictNonConstructiveConjunctionReductioVariations = [
                 [ SA (lneg $ phin 1) :|-: SS (lneg $ phin 1) 
-                , GammaV 1 :+: SA (phin 1) :|-: SS (phin 2 .∧. (lneg $ phin 2))
+                , GammaV 1 :+: SA (lneg $ phin 1) :|-: SS (phin 2 .∧. (lneg $ phin 2))
                 ] ∴ GammaV 1 :|-: SS (phin 1)
             ,
                 [ SA (lneg $ phin 1) :|-: SS (lneg $ phin 1) 
