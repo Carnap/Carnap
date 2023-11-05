4548b344e3e240398ba7053ec87e8aa3e9d265f7
Mang Yau <28640369+mangoes-git@users.noreply.github.com>
2023-11-05 14:33:16 -0500

    CARNAP-143 Reset button for Syntax Check problems (#6)
    * initial draft
    
    * implement reset button for Syntax Checking

--- Carnap-GHCJS/src/Carnap/GHCJS/Action/SyntaxCheck.hs
+++ Carnap-GHCJS/src/Carnap/GHCJS/Action/SyntaxCheck.hs
@@ -155,10 +155,32 @@ activateChecker w (Just (i,o,opts)) =
                          (Just w') <- getDefaultView w
                          doOnce i keyUp False $ liftIO $ btStatus Edited
                          setAttribute i "enterKeyHint" "go"
-                         addListener i keyUp match False                  
+                         addListener i keyUp match False
+                         resetButton <- questionButton w "Reset"
+                         appendChild bw (Just resetButton)
+                         resetGoal <- newListener $ resetGoalWrapper tree ref sf
+                         addListener resetButton click resetGoal False
                       (Left e) -> setInnerHTML o (Just $ show e)
                   _ -> print "syntax check was missing an option"
 activateChecker _ Nothing  = return ()
+
+
+resetGoalWrapper :: Element
+                    -> IORef (PureForm, [(PureForm, Int)], Tree (PureForm, Int), Int)
+                    -> (PureForm -> String)
+                    -> EventM Element MouseEvent ()
+resetGoalWrapper o ref sf = liftIO $ do
+    resetFn o ref sf
+
+resetFn :: Element
+                 -> IORef (PureForm, [(PureForm, Int)], Tree (PureForm, Int), Int)
+                 -> (PureForm -> String)
+                 -> IO ()
+resetFn o ref sf = do
+    (f, _, _, _) <- liftIO $ readIORef ref
+    liftIO $ writeIORef ref (f, [(f, 0)], T.Node (f, 0) [], 0)
+    liftIO $ setInnerHTML o (Just $ sf f)
+
 
 submitSyn :: IsEvent e => Document -> M.Map String String -> IORef (PureForm,[(PureForm,Int)], Tree (PureForm,Int),Int) -> String -> EventM HTMLInputElement e ()
 submitSyn w opts ref l = do (f,forms,_,_) <- liftIO $ readIORef ref
