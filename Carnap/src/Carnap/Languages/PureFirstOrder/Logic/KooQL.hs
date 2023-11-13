fc5d6d29bdcada9dedc9801263258c2835475e58
Alex Pejovic <62117049+alexpejovic@users.noreply.github.com>
2023-11-13 15:04:51 -0500

    CARNAP-115/repeat-line-rule (#10)
    * Added rule "R" ot KooKMM-SL
    
    * Add R rule to KooQL
    
    * Fixed compile-time bug

--- Carnap/src/Carnap/Languages/PureFirstOrder/Logic/KooQL.hs
+++ Carnap/src/Carnap/Languages/PureFirstOrder/Logic/KooQL.hs
@@ -25,11 +25,97 @@ import Carnap.Languages.PurePropositional.Util
 import Carnap.Languages.PureFirstOrder.Parser
 import Carnap.Languages.PureFirstOrder.Syntax
 import Carnap.Languages.PureFirstOrder.Logic.Rules
-import Carnap.Languages.PureFirstOrder.Logic.KalishAndMontague
 
 --------------------------------------------------------
 --2. Classical First-Order Logic
 --------------------------------------------------------
+
+data KooQL =  SL KooSL
+                | UD  | UI  | EG  | EI | QN1 | QN2  | QN3  | QN4  
+                | LL1 | LL2 | EL1 | EL2 | ID  | SM  | ALL1 | ALL2
+                | DER (ClassicalSequentOver PureLexiconFOL (Sequent (Form Bool)))
+                | PR (Maybe [(ClassicalSequentOver PureLexiconFOL (Sequent (Form Bool)))])
+               deriving (Eq)
+
+instance Show KooQL where
+        show (PR _)  = "PR"
+        show UD      = "UD"
+        show UI      = "UI"
+        show EG      = "EG"
+        show EI      = "EI"
+        show (DER _) = "Derived"
+        show QN1     = "QN"
+        show QN2     = "QN"
+        show QN3     = "QN"
+        show QN4     = "QN"
+        show ID      = "Id"
+        show LL1     = "LL"
+        show LL2     = "LL"
+        show ALL1    = "LL"
+        show ALL2    = "LL"
+        show EL1     = "EL"
+        show EL2     = "EL"
+        show SM      = "Sm"
+        show (SL x)  = show x
+
+instance Inference KooQL PureLexiconFOL (Form Bool) where
+     ruleOf (PR _)    = axiom
+     ruleOf UI        = universalInstantiation
+     ruleOf EG        = existentialGeneralization
+     ruleOf UD        = universalGeneralization
+     ruleOf EI        = existentialInstantiation
+     ruleOf QN1       = quantifierNegation !! 0
+     ruleOf QN2       = quantifierNegation !! 1
+     ruleOf QN3       = quantifierNegation !! 2
+     ruleOf QN4       = quantifierNegation !! 3
+     ruleOf LL1       = leibnizLawVariations !! 0
+     ruleOf LL2       = leibnizLawVariations !! 1
+     ruleOf ALL1      = antiLeibnizLawVariations !! 0
+     ruleOf ALL2      = antiLeibnizLawVariations !! 1
+     ruleOf EL1       = euclidsLawVariations !! 0
+     ruleOf EL2       = euclidsLawVariations !! 1
+     ruleOf ID        = eqReflexivity
+     ruleOf SM        = eqSymmetry
+
+     premisesOf (SL x) = map liftSequent (premisesOf x)
+     premisesOf (DER r) = multiCutLeft r
+     premisesOf x     = upperSequents (ruleOf x)
+
+     conclusionOf (SL x) = liftSequent (conclusionOf x)
+     conclusionOf (DER r) = multiCutRight r
+     conclusionOf x   = lowerSequent (ruleOf x)
+
+     restriction (PR prems) = Just (premConstraint prems)
+     restriction _          = Nothing
+     
+     globalRestriction (Left ded) n UD = Just (montagueNewUniversalConstraint [tau] ded n)
+     globalRestriction (Left ded) n EI = Just (montagueNewExistentialConstraint [tau] ded n)
+     globalRestriction _ _ _ = Nothing
+
+     indirectInference (SL x) = indirectInference x
+     indirectInference UD  = Just PolyProof
+     indirectInference _ = Nothing
+
+parseKooQL :: RuntimeDeductionConfig PureLexiconFOL (Form Bool) -> Parsec String u [KooQL]
+parseKooQL rtc = try quantRule <|> liftProp
+    where liftProp = do r <- parseKooSL (defaultRuntimeDeductionConfig)
+                        return (map SL r)
+          quantRule = do r <- choice (map (try . string) ["PR", "UI", "UD", "EG", "EI", "QN","LL","EL","Id","Sm","D-"])
+                         case r of 
+                            r | r == "PR" -> return [PR $ problemPremises rtc]
+                              | r == "UI" -> return [UI]
+                              | r == "UD" -> return [UD]
+                              | r == "EG" -> return [EG]
+                              | r == "EI" -> return [EI]
+                              | r == "QN" -> return [QN1,QN2,QN3,QN4]
+                              | r == "LL" -> return [LL1,LL2,ALL1,ALL2]
+                              | r == "Sm" -> return [SM]
+                              | r == "EL" -> return [EL1,EL2]
+                              | r == "Id" -> return [ID]
+                              | r == "D-" ->  do rn <- many1 upper
+                                                 case M.lookup rn (derivedRules rtc) of
+                                                    Just r  -> return [DER r]
+                                                    Nothing -> parserFail "Looks like you're citing a derived rule that doesn't exist"
 
 kooQLParserOptions :: FirstOrderParserOptions PureLexiconFOL u Identity
 kooQLParserOptions = FirstOrderParserOptions 
@@ -78,8 +164,8 @@ kooParserFromOptions opts = do  f <- buildExpressionParser (opTable opts) subfor
 kooQLFormulaParser :: Parsec String u PureFOLForm
 kooQLFormulaParser = kooParserFromOptions kooQLParserOptions
 
-parseKooQLProof ::  RuntimeDeductionConfig PureLexiconFOL (Form Bool) -> String -> [DeductionLine MontagueQCCalc PureLexiconFOL (Form Bool)]
-parseKooQLProof ders = toDeductionMontague (parseMontagueQCCalc ders) kooQLFormulaParser
+parseKooQLProof ::  RuntimeDeductionConfig PureLexiconFOL (Form Bool) -> String -> [DeductionLine KooQL PureLexiconFOL (Form Bool)]
+parseKooQLProof ders = toDeductionMontague (parseKooQL ders) kooQLFormulaParser
 
 kooQLCalc = mkNDCalc
     { ndRenderer = MontagueStyle
--- Carnap/src/Carnap/Languages/PurePropositional/Logic/KooSL.hs
+++ Carnap/src/Carnap/Languages/PurePropositional/Logic/KooSL.hs
@@ -1,6 +1,6 @@
 {-#LANGUAGE FlexibleContexts, FlexibleInstances, MultiParamTypeClasses #-}
 module Carnap.Languages.PurePropositional.Logic.KooSL
-    (parseKooSLProof, kooSLCalc, kooSLNotation) where
+    (parseKooSLProof, kooSLCalc, kooSLNotation, KooSL, parseKooSL) where
 
 import Data.Map as M (lookup, Map)
 import Text.Parsec
@@ -15,14 +15,119 @@ import Carnap.Calculi.NaturalDeduction.Checker
 import Carnap.Languages.ClassicalSequent.Syntax
 import Carnap.Languages.ClassicalSequent.Parser
 import Carnap.Languages.PurePropositional.Logic.Rules
-import Carnap.Languages.PurePropositional.Logic.KalishAndMontague (parseMontagueSC, MontagueSC)
 
 --A system for propositional logic resembling the proof system from Kalish
 --and Montague's LOGIC, with derived rules, adding Prof. Alex Koo's requested edits.
 
+data KooSL = PR (Maybe [(ClassicalSequentOver PurePropLexicon (Sequent (Form Bool)))])
+               | MP  | MT  | DNE | DNI  | DD   | AS   
+               | CP1 | CP2 | ID1 | ID2  | ID3  | ID4  | ID5  | ID6 | ID7 | ID8
+               | ADJ | S1  | S2  | ADD1 | ADD2 | MTP1 | MTP2 | BC1 | BC2 | CB | R
+               | DER (ClassicalSequentOver PurePropLexicon (Sequent (Form Bool)))
+               deriving (Eq)
+
+instance Show KooSL where
+    show MP      = "MP"
+    show MT      = "MT"
+    show DNE     = "DNE"
+    show DNI     = "DNI"
+    show DD      = "DD"
+    show AS      = "AS"
+    show CP1     = "CD"
+    show CP2     = "CD"
+    show ID1     = "ID"
+    show ID2     = "ID"
+    show ID3     = "ID"
+    show ID4     = "ID"
+    show ID5     = "ID"
+    show ID6     = "ID"
+    show ID7     = "ID"
+    show ID8     = "ID"
+    show ADJ     = "ADJ"
+    show S1      = "S"
+    show S2      = "S"
+    show ADD1    = "ADD"
+    show ADD2    = "ADD"
+    show MTP1    = "MTP"
+    show MTP2    = "MTP"
+    show BC1     = "BC"
+    show BC2     = "BC"
+    show CB      = "CB"
+    show R       = "R"
+    show (PR _)  = "PR"
+    show (DER _) = "Derived"
+
+instance Inference KooSL PurePropLexicon (Form Bool) where
+    ruleOf MP        = modusPonens
+    ruleOf MT        = modusTollens
+    ruleOf AS        = axiom
+    ruleOf (PR _)    = axiom
+    ruleOf ID1       = constructiveReductioVariations !! 0
+    ruleOf ID2       = constructiveReductioVariations !! 1
+    ruleOf ID3       = constructiveReductioVariations !! 2
+    ruleOf ID4       = constructiveReductioVariations !! 3
+    ruleOf ID5       = nonConstructiveReductioVariations !! 0
+    ruleOf ID6       = nonConstructiveReductioVariations !! 1
+    ruleOf ID7       = nonConstructiveReductioVariations !! 2
+    ruleOf ID8       = nonConstructiveReductioVariations !! 3
+    ruleOf DD        = identityRule
+    ruleOf DNE       = doubleNegationElimination
+    ruleOf DNI       = doubleNegationIntroduction
+    ruleOf CP1       = conditionalProofVariations !! 0
+    ruleOf CP2       = conditionalProofVariations !! 1
+    ruleOf ADJ       = adjunction
+    ruleOf S1        = simplificationVariations !! 0
+    ruleOf S2        = simplificationVariations !! 1
+    ruleOf ADD1      = additionVariations !! 0
+    ruleOf ADD2      = additionVariations !! 1
+    ruleOf MTP1      = modusTollendoPonensVariations !! 0
+    ruleOf MTP2      = modusTollendoPonensVariations !! 1
+    ruleOf BC1       = biconditionalToConditionalVariations !! 0
+    ruleOf BC2       = biconditionalToConditionalVariations !! 1
+    ruleOf CB        = conditionalToBiconditional
+    ruleOf R         = identityRule
+
+    premisesOf (DER r) = multiCutLeft r
+    premisesOf r = upperSequents (ruleOf r)
+
+    conclusionOf (DER r) = multiCutRight r
+    conclusionOf r = lowerSequent (ruleOf r)
+
+    restriction (PR prems) = Just (premConstraint prems)
+    restriction _ = Nothing
+
+    indirectInference x
+        | x `elem` [DD,CP1,CP2,ID1,ID2,ID3,ID4] = Just PolyProof
+        | otherwise = Nothing
+
+parseKooSL :: RuntimeDeductionConfig PurePropLexicon (Form Bool) -> Parsec String u [KooSL]
+parseKooSL rtc = do r <- choice (map (try . string) ["AS","PR","MP","MTP","MT","DD","DNE","DNI", "DN", "S", "ADJ",  "ADD" , "BC", "CB",  "CD", "ID", "R", "D-"])
+                    case r of
+                        "AS"   -> return [AS]
+                        "PR"   -> return [PR (problemPremises rtc)]
+                        "MP"   -> return [MP]
+                        "MT"   -> return [MT]
+                        "DD"   -> return [DD]
+                        "DNE"  -> return [DNE]
+                        "DNI"  -> return [DNI]
+                        "DN"   -> return [DNE,DNI]
+                        "CD"   -> return [CP1,CP2]
+                        "ID"   -> return [ID1,ID2,ID3,ID4,ID5,ID6,ID7,ID8]
+                        "ADJ"  -> return [ADJ]
+                        "S"    -> return [S1, S2]
+                        "ADD"  -> return [ADD1, ADD2]
+                        "MTP"  -> return [MTP1, MTP2]
+                        "BC"   -> return [BC1, BC2]
+                        "CB"   -> return [CB]
+                        "R"    -> return [R]
+                        "D-" -> do  rn <- many1 upper
+                                    case M.lookup rn (derivedRules rtc) of
+                                        Just r  -> return [DER r]
+                                        Nothing -> parserFail "Looks like you're citing a derived rule that doesn't exist"
+
 parseKooSLProof :: RuntimeDeductionConfig PurePropLexicon (Form Bool) 
-                     -> String -> [DeductionLine MontagueSC PurePropLexicon (Form Bool)]
-parseKooSLProof rtc = toDeductionMontague (parseMontagueSC rtc) (kooSLFormulaParser kooOpts)
+                     -> String -> [DeductionLine KooSL PurePropLexicon (Form Bool)]
+parseKooSLProof rtc = toDeductionMontague (parseKooSL rtc) (kooSLFormulaParser kooOpts)
 
 kooSLNotation :: String -> String
 kooSLNotation x = case runParser altParser 0 "" x of
